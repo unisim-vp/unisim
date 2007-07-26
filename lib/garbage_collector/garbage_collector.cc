f884362973f9b47d5c602e64f68ed1acf01ccdc9
mouchard <mouchard@ea533459-3c0a-0410-a024-a90cf4b01ff5>
2007-07-26 16:39:52 +0000

    git-svn-id: https://unisim.org/svn/devel@5017 ea533459-3c0a-0410-a024-a90cf4b01ff5

---
+++ lib/garbage_collector/garbage_collector.cc
@@ -0,0 +1,162 @@
+/*
+ *  Copyright (c) 2007,
+ *  Commissariat a l'Energie Atomique (CEA)
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification,
+ *  are permitted provided that the following conditions are met:
+ *
+ *   - Redistributions of source code must retain the above copyright notice, this
+ *     list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of CEA nor the names of its contributors may be used to
+ *     endorse or promote products derived from this software without specific prior
+ *     written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED.
+ *  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ *  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ *  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ *  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Authors: Gilles Mouchard (gilles.mouchard@cea.fr)
+ */
+ 
+#include <lib/garbage_collector/garbage_collector.hh>
+#include <stdlib.h>
+
+using std::cerr;
+using std::endl;
+
+void *operator new(std::size_t size, full_system::utils::garbage_collector::PointerBase)
+{
+//	cerr << "GarbageCollector::operator new " << size << endl;
+	return full_system::utils::garbage_collector::GarbageCollector::Allocate(size);
+}
+
+void *operator new[](std::size_t size, full_system::utils::garbage_collector::PointerBase)
+{
+//	cerr << "GarbageCollector::operator new[] " << size << endl;
+	return full_system::utils::garbage_collector::GarbageCollector::Allocate(size);
+}
+
+namespace unisim {
+namespace lib {
+namespace garbage_collector {
+
+MemoryBlock *GarbageCollector::free_blocks[GarbageCollector::MAX_MEMORY_BLOCK_SIZE / GarbageCollector::MIN_MEMORY_BLOCK_SIZE];
+
+void GarbageCollector::AtExit() {
+	Reset();
+}
+
+void GarbageCollector::Setup()
+{
+	atexit(AtExit);
+	memset(free_blocks, 0, sizeof(free_blocks));
+}
+
+void GarbageCollector::Reset()
+{
+	unsigned int i;
+//	cerr << "GarbageCollector::Reset()" << endl;
+	for(i = 0; i < MAX_MEMORY_BLOCK_SIZE / MIN_MEMORY_BLOCK_SIZE; i++)
+	{
+		MemoryBlock * block = free_blocks[i];
+		if(block)
+		{
+			MemoryBlock *next_block;
+			do
+			{
+				next_block = block->next;
+				//cerr << "GarbageCollector::Reset() free" << endl;
+				free(block);
+			} while((block = next_block) != 0);
+		}
+		free_blocks[i] = 0;
+	}
+}
+
+void *GarbageCollector::Allocate(unsigned int size)
+{
+	MemoryBlock *block;
+	unsigned int index = size/ MIN_MEMORY_BLOCK_SIZE;
+	if(index >= MAX_MEMORY_BLOCK_SIZE / MIN_MEMORY_BLOCK_SIZE)
+	{
+		block = (MemoryBlock *) malloc(sizeof(MemoryBlock) + size);
+		block->magic = MAGIC;
+		block->refcount = 0;
+		block->size = size;
+		
+//		cerr << "GarbageCollector::Allocate(" << size << ") on big blocks: " << block << endl;
+
+ 		return block + 1;
+	}
+		
+	if(free_blocks[index])
+	{
+		block = free_blocks[index];
+		free_blocks[index] = block->next;
+		
+//		cerr << "GarbageCollector::Allocate(" << size << ") on free blocks: " << block << endl;		
+ 		return block + 1;
+	}
+//		(index + 1) * MIN_MEMORY_BLOCK_SIZE
+	block = (MemoryBlock *) malloc(sizeof(MemoryBlock) + ((index + 1) * MIN_MEMORY_BLOCK_SIZE));
+//	block = (MemoryBlock *) malloc(sizeof(MemoryBlock) + ((size + MIN_MEMORY_BLOCK_SIZE - 1) & ~(MIN_MEMORY_BLOCK_SIZE - 1)));
+	block->magic = MAGIC;
+	block->refcount = 0;
+	block->size = size;
+//	cerr << "GarbageCollector::Allocate(" << size << ") on a new block: " << block << endl;
+	
+	return block + 1;
+}
+
+bool GarbageCollector::Collect(void *p)
+{
+	MemoryBlock *block = (MemoryBlock *) p - 1;
+	if(block->magic != MAGIC)
+	{
+		cerr << "WARNING! Doing collect on unmanaged block" << endl;
+		abort();
+	}
+	//cerr << "GarbageCollector::Collect(0x" << p << "): refcount=" << block->refcount << endl;
+	if(--block->refcount != 0) return false;
+	unsigned int index = block->size/ MIN_MEMORY_BLOCK_SIZE;
+	if(index >= MAX_MEMORY_BLOCK_SIZE / MIN_MEMORY_BLOCK_SIZE)
+	{
+		free(block);
+	}
+	else
+	{
+		block->next = free_blocks[index];
+		free_blocks[index] = block;
+	}
+	return true;
+}
+
+void GarbageCollector::Catch(void *p)
+{
+	MemoryBlock *block = (MemoryBlock *) p - 1;
+	if(block->magic != MAGIC)
+	{
+		cerr << "WARNING! Doing catch on unmanaged block" << endl;
+		abort();
+	}
+	//cerr << "GarbageCollector::Catch(0x" << p << "): refcount=" << block->refcount << endl;
+	block->refcount++;
+}
+
+} // end of namespace garbage_collector
+} // end of namespace lib
+} // end of namespace unisim
---
+++ lib/garbage_collector/garbage_collector.hh
@@ -0,0 +1,280 @@
+/*
+ *  Copyright (c) 2007,
+ *  Commissariat a l'Energie Atomique (CEA)
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification,
+ *  are permitted provided that the following conditions are met:
+ *
+ *   - Redistributions of source code must retain the above copyright notice, this
+ *     list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of CEA nor the names of its contributors may be used to
+ *     endorse or promote products derived from this software without specific prior
+ *     written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED.
+ *  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ *  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ *  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ *  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Authors: Gilles Mouchard (gilles.mouchard@cea.fr)
+ */
+ 
+#ifndef __UNISIM_LIB_GARBAGE_COLLECTOR_GARBAGE_COLLECTOR_HH__
+#define __UNISIM_LIB_GARBAGE_COLLECTOR_GARBAGE_COLLECTOR_HH__
+
+#include <iostream>
+#include <stdlib.h>
+
+namespace unisim {
+namespace lib {
+namespace garbage_collector {
+
+class PointerBase { };
+
+} // end of namespace garbage_collector
+} // end of namespace lib
+} // end of namespace unisim
+
+void *operator new(std::size_t size, full_system::utils::garbage_collector::PointerBase);
+void *operator new[](std::size_t size, full_system::utils::garbage_collector::PointerBase);
+
+namespace unisim {
+namespace lib {
+namespace garbage_collector {
+
+using std::cerr;
+using std::endl;	
+	
+struct MemoryBlock
+{
+	unsigned int magic;
+	unsigned int refcount;
+	unsigned int size;
+	MemoryBlock *next;
+};
+
+class GarbageCollector
+{
+public:
+	static void Setup();
+	static void Reset();
+
+	static /* inline */ bool Collect(void *p)
+#if 0 // defined(__GNUC__) && (__GNUC__ >= 3)
+__attribute__((always_inline))
+#endif
+	;
+
+	static /* inline */ void *Allocate(unsigned int size)
+#if 0 // defined(__GNUC__) && (__GNUC__ >= 3)
+__attribute__((always_inline))
+#endif
+	;
+
+	static /* inline */ void Catch(void *p)
+#if 0 // defined(__GNUC__) && (__GNUC__ >= 3)
+__attribute__((always_inline))
+#endif
+	;
+	
+private:
+	static void AtExit();
+	static const unsigned int MIN_MEMORY_BLOCK_SIZE = 32;
+	static const unsigned int MAX_MEMORY_BLOCK_SIZE = 32 * MIN_MEMORY_BLOCK_SIZE;
+	static const unsigned int MAGIC = 0x5f47435f; // '_GC_';
+	
+	static MemoryBlock *free_blocks[MAX_MEMORY_BLOCK_SIZE / MIN_MEMORY_BLOCK_SIZE];
+};
+
+template <class T>
+class Pointer : public PointerBase
+{
+public:
+	inline Pointer()
+#if defined(__GNUC__) && (__GNUC__ >= 3)
+__attribute__((always_inline))
+#endif
+	;
+
+	inline Pointer(const Pointer<T>& ptr)
+#if defined(__GNUC__) && (__GNUC__ >= 3)
+__attribute__((always_inline))
+#endif
+	;
+
+	inline Pointer(T *_obj)
+#if defined(__GNUC__) && (__GNUC__ >= 3)
+__attribute__((always_inline))
+#endif
+	;
+
+	inline ~Pointer()
+#if defined(__GNUC__) && (__GNUC__ >= 3)
+__attribute__((always_inline))
+#endif
+	;
+
+	inline bool operator == (const Pointer<T>& p) const
+#if defined(__GNUC__) && (__GNUC__ >= 3)
+__attribute__((always_inline))
+#endif
+	;
+
+// 	inline operator void * () const
+// #if defined(__GNUC__) && (__GNUC__ >= 3)
+// __attribute__((always_inline))
+// #endif
+// 	;
+
+// 	inline T* operator * () const
+// #if defined(__GNUC__) && (__GNUC__ >= 3)
+// __attribute__((always_inline))
+// #endif
+// 	;
+
+	inline operator T * () const
+#if defined(__GNUC__) && (__GNUC__ >= 3)
+__attribute__((always_inline))
+#endif
+	;
+
+	inline T *operator -> () const
+#if defined(__GNUC__) && (__GNUC__ >= 3)
+__attribute__((always_inline))
+#endif
+	;
+
+	inline Pointer<T>& operator = (T *_obj)
+#if defined(__GNUC__) && (__GNUC__ >= 3)
+__attribute__((always_inline))
+#endif
+	;
+
+	inline Pointer<T>& operator = (const Pointer<T>& ptr)
+#if defined(__GNUC__) && (__GNUC__ >= 3)
+__attribute__((always_inline))
+#endif
+	;
+	
+	inline T& operator [](unsigned int index)
+#if defined(__GNUC__) && (__GNUC__ >= 3)
+__attribute__((always_inline))
+#endif
+	;
+
+private:
+	T *obj;
+};
+
+template <class T>
+inline Pointer<T>::Pointer() : obj(0)
+{
+}
+
+template <class T>
+inline Pointer<T>::Pointer(const Pointer<T>& ptr) : obj(ptr.obj)
+{
+	if(obj)
+		GarbageCollector::Catch(obj);
+}
+
+template <class T>
+inline Pointer<T>::Pointer(T *_obj) : obj(_obj)
+{
+	if(obj)
+		GarbageCollector::Catch(obj);
+}
+
+template <class T>
+inline Pointer<T>::~Pointer()
+{
+	if(obj)
+	{
+		if(GarbageCollector::Collect(obj))
+			obj->~T();
+	}
+}
+
+template <class T>
+inline bool Pointer<T>::operator == (const Pointer<T>& p) const
+{
+	return obj == p.obj;
+}
+
+// template <class T>
+// inline Pointer<T>::operator void * () const
+// {
+// 	return obj;
+// }
+
+// template <class T>
+// inline T* Pointer<T>::operator * () const
+// {
+// 	return obj;
+// }
+
+template <class T>
+inline Pointer<T>::operator T * () const
+{
+	return obj;
+}
+
+template <class T>
+inline T *Pointer<T>::operator -> () const
+{
+	return obj;
+}
+
+template <class T>
+inline Pointer<T>& Pointer<T>::operator = (T *_obj)
+{
+	if(obj)
+	{
+		if(GarbageCollector::Collect(obj))
+			obj->~T();
+	}
+
+	obj = _obj;
+	
+	if(obj)
+		GarbageCollector::Catch(obj);
+}
+
+template <class T>
+inline Pointer<T>& Pointer<T>::operator = (const Pointer<T>& ptr)
+{
+	if(obj)
+	{
+		if(GarbageCollector::Collect(obj))
+			obj->~T();
+	}
+	
+	obj = ptr.obj;
+	
+	if(obj)
+		GarbageCollector::Catch(obj);
+}
+
+template <class T>
+inline T& Pointer<T>::operator [](unsigned int index) {
+	return obj[index];
+} 
+
+} // end of namespace garbage_collector
+} // end of namespace lib
+} // end of namespace unisim
+
+#endif
