6c2ab7e2abca7dcee09075527a36f301ea3be5c3
mouchard <mouchard@ea533459-3c0a-0410-a024-a90cf4b01ff5>
2007-08-01 15:01:25 +0000

    git-svn-id: https://unisim.org/svn/devel@5124 ea533459-3c0a-0410-a024-a90cf4b01ff5

--- build-configure.sh
+++ build-configure.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export m4_path="`pwd`/m4"
 echo "doing aclocal -I$m4_path; autoconf; autoheader; automake -a in"
-find . -name "configure.ac" -execdir sh -c "pwd; aclocal -I\$m4_path; autoconf; has_ac_config_headers=\`grep AC_CONFIG_HEADERS configure.ac\`; if test \"x\$has_ac_config_headers\" != \"x\"; then autoheader; fi; automake -a" \;
+find . -name "configure.ac" -execdir sh -c "pwd; aclocal -I \$m4_path; autoconf; has_ac_config_headers=\`grep AC_CONFIG_HEADERS configure.ac\`; if test \"x\$has_ac_config_headers\" != \"x\"; then autoheader; fi; automake -a" \;
