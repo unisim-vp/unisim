ddee78e92008bb9c4212347ac44b48f636d0637e
mouchard <mouchard@ea533459-3c0a-0410-a024-a90cf4b01ff5>
2007-09-24 07:10:08 +0000

    Fixed build-package-unisim-simulators description.
    git-svn-id: https://unisim.org/svn/devel@5465 ea533459-3c0a-0410-a024-a90cf4b01ff5

--- package/deb/build-package-unisim-simulators.sh
+++ package/deb/build-package-unisim-simulators.sh
@@ -10,6 +10,6 @@ VERSION="1.0-1" \
 ARCH="i386" \
 DEPS="libncurses5 (>= 5.5), libreadline5 (>= 5.2), libxml2 (>= 2.6.27), libsdl1.2debian (>= 1.2.11),  libstdc++6 (>= 4.1.2), libc6 (>= 2.5), libgcc1 (>= 4.1.2)" \
 MAINTAINER="Gilles Mouchard <gilles.mouchard@cea.fr>" \
-DESCRIPTION="UNISIM library" \
+DESCRIPTION="UNISIM Simulators" \
 INSTALL_PATH="usr" \
 ./build-package.sh --host=i686-pc-linux-gnu "CXX=g++" "--with-unisim-lib=$1" "--with-systemc=$2" "CXXFLAGS=-m32 -O3 -g3 -DSC_INCLUDE_DYNAMIC_PROCESSES" "CPPFLAGS=-I/usr/include/libxml2"
