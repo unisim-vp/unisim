b07f234236a66e0e8dc1bc9db8c570991e85b2ec
nouacer <nouacer@ea533459-3c0a-0410-a024-a90cf4b01ff5>
2008-04-28 12:15:49 +0000

    68HCS12X:: add The Words Copyright to the project files.
    git-svn-id: https://unisim.org/svn/devel@7901 ea533459-3c0a-0410-a024-a90cf4b01ff5

--- unisim_lib/unisim/component/cxx/processor/hcs12x/config.hh
+++ unisim_lib/unisim/component/cxx/processor/hcs12x/config.hh
@@ -32,74 +32,44 @@
  * Authors: Reda   Nouacer  (reda.nouacer@cea.fr)
  */
 
-#include <mmc.hh>
-#include <stdio.h>
-#include <assert.h>
+
+#ifndef __UNISIM_COMPONENT_CXX_PROCESSOR_HCS12X_CONFIG_HH__
+#define __UNISIM_COMPONENT_CXX_PROCESSOR_HCS12X_CONFIG_HH__
+
+#ifdef GCC_INLINE
+#undef GCC_INLINE
+#endif
+
+#if defined(__GNUC__) && (__GNUC__ >= 3)
+#define GCC_INLINE __attribute__((always_inline))
+#else
+#define GCC_INLINE
+#endif
 
 namespace unisim {
 namespace component {
 namespace cxx {
 namespace processor {
 namespace hcs12x {
-	
-MMC::MMC(uint8_t gpage, uint8_t rpage, uint8_t epage, uint8_t ppage, uint8_t direct) {
-    setGpage(gpage);    
-    setRpage(rpage);
-    setEpage(epage);
-    setPpage(ppage);
-    setDirect(direct);
-    
-}
+
+struct CONFIG {
+	/*
+	 * static initialization may rise problems in SMP architectures !!!
+	 */
+	static const bool DEBUG_ENABLE = true;
+	static const bool DEBUG_EXCEPTION_ENABLE = false;
+	static const bool HAS_HARD_RESET = false;
+	static const bool HAS_SOFT_RESET = false;
+	static const bool HAS_EXTERNAL_INTERRUPT = false;
+
+};
 
 
-physical_address_t MMC::getPhysicalAddress(address_t logicalAddress, MEMORY::MAP type) {
-// TODO:
-// understand how to use mmc during compute of the physicalAddress !!!!
-	
-	physical_address_t address = 0;
-	
-	switch (type) {
-		case MEMORY::EXTENDED: { 
-			address = logicalAddress; 
-		} break;
-		case MEMORY::DIRECT: {
-			address = getDirectAddress((uint8_t) logicalAddress); 
-		} break;
-	}
-	return address;
-}
+} // end of namespace hcs12x
+} // end of namespace processor
+} // end of namespace cxx
+} // end of namespace component
+} // end of namespace unisim
 
-void MMC::setGpage (uint8_t val) { _gpage = val;}
-uint8_t MMC::getGpage () { return _gpage; }
-
-void MMC::setDirect (uint8_t val) { 
- 
-	if (!_isDirectSet) {
-    	_direct = val; 
-    	_isDirectSet = true;
-	} 
-}
-uint8_t MMC::getDirect () { return _direct; }
-
-address_t MMC::getDirectAddress(uint8_t lowByte) {
-
-	return (((address_t) getDirect()) << 8) | ((address_t) lowByte & 0x00FF);
-}
-
-
-void MMC::setRpage (uint8_t val) { _rpage = val;}
-uint8_t MMC::getRpage () { return _rpage; }
-
-void MMC::setEpage (uint8_t val) { _epage = val;}
-uint8_t MMC::getEpage () { return _epage; }
-
-void MMC::setPpage (uint8_t val) { _ppage = val;}
-uint8_t MMC::getPpage () { return _ppage; }
-
-	
-}
-}
-}
-}
-} // end namespace hcs12x
+#endif // __UNISIM_COMPONENT_CXX_PROCESSOR_HCS12X_CONFIG_HH__
 
--- unisim_lib/unisim/component/cxx/processor/hcs12x/cpu.hh
+++ unisim_lib/unisim/component/cxx/processor/hcs12x/cpu.hh
@@ -1,3 +1,37 @@
+/*
+ *  Copyright (c) 2008,
+ *  Commissariat a l'Energie Atomique (CEA)
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification,
+ *  are permitted provided that the following conditions are met:
+ *
+ *   - Redistributions of source code must retain the above copyright notice, this
+ *     list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of CEA nor the names of its contributors may be used to
+ *     endorse or promote products derived from this software without specific prior
+ *     written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED.
+ *  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ *  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ *  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ *  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Authors: Reda   Nouacer  (reda.nouacer@cea.fr)
+ */
+
 #ifndef __UNISIM_COMPONENT_CXX_PROCESSOR_HCS12X_CPU_HH__
 #define __UNISIM_COMPONENT_CXX_PROCESSOR_HCS12X_CPU_HH__
 
@@ -31,7 +65,7 @@
 #include <map>
 #include <inttypes.h>
 
-
+#include <unisim/component/cxx/processor/hcs12x/config.hh>
 #include <unisim/component/cxx/processor/hcs12x/ccr.hh>
 #include <unisim/component/cxx/processor/hcs12x/mmc.hh>
 #include <unisim/component/cxx/processor/hcs12x/types.hh>
@@ -92,19 +126,6 @@ using std::vector;
 
 
 class CPU;
-
-struct CONFIG {
-	/*
-	 * static initialization may rise problems in SMP architectures !!!
-	 */
-	static const bool DEBUG_ENABLE = true;
-	static const bool DEBUG_EXCEPTION_ENABLE = false;
-	static const bool HAS_HARD_RESET = false;
-	static const bool HAS_SOFT_RESET = false;
-	static const bool HAS_EXTERNAL_INTERRUPT = false;
-
-};
-
 
 	/* *******************************************************************
 	 * ********  Used for                                   **************
