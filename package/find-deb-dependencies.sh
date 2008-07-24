a73380cfab6e87936b314c8778f68fb77492e6ed
mouchard <mouchard@ea533459-3c0a-0410-a024-a90cf4b01ff5>
2008-07-24 13:35:45 +0000

    git-svn-id: https://unisim.org/svn/devel@9132 ea533459-3c0a-0410-a024-a90cf4b01ff5

--- package/find-deb-dependencies.sh
+++ package/find-deb-dependencies.sh
@@ -2,16 +2,15 @@
 if [ "x$1" = x ]; then
 	echo "`basename $0`: Find package dependencies of a dynamically linked ELF binary."
 	echo "Usage: `basename $0` <filename>"
-	exit 0
+	exit 1
 fi
 
 FILE=$1
-OUTPUT=
-SO_DEPS=
 if [ -f ${FILE} ] && [ -x ${FILE} ]; then
 	FILE_HEAD=`head -c 4 ${FILE}`
 	ELF_HEAD=`echo -e "\0177ELF"`
 	if [ "${FILE_HEAD}" = "${ELF_HEAD}" ]; then
+		OUTPUT=
 		SO_DEPS=`ldd ${FILE} | sed 's/^.*=>//' | cut -f 1 -d "("`
 		#echo "SO_DEPS=\"${SO_DEPS}\""
 		for SO_DEP in ${SO_DEPS}; do
@@ -27,10 +26,10 @@ if [ -f ${FILE} ] && [ -x ${FILE} ]; then
 			done
 		done
 
-	else
-		echo "${FILE} is not an ELF binary"
+		echo -e "${OUTPUT}" | sort -u
 		exit 0
 	fi
 fi
 
-echo -e "${OUTPUT}" | sort -u
+echo "${FILE} is not an ELF binary"
+exit 1
