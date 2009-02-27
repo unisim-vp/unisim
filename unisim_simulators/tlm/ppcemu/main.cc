152deba73f4549b7f0c2637a20ffddde4a79da99
gracia <gracia@ea533459-3c0a-0410-a024-a90cf4b01ff5>
2009-02-27 00:26:15 +0000

    Removed connection from the linux_os to the old logger service. Now logger is part of the UNISIM kernel, so there is no need to connect modules to it, it is just there.
    git-svn-id: https://unisim.org/svn/devel@10859 ea533459-3c0a-0410-a024-a90cf4b01ff5

--- unisim_simulators/tlm/ppcemu/main.cc
+++ unisim_simulators/tlm/ppcemu/main.cc
@@ -629,7 +629,7 @@ int sc_main(int argc, char *argv[])
 		fsb_to_mem_bridge->logger_import >> *logger->logger_export[logger_index++];
 		memory->logger_import >> *logger->logger_export[logger_index++];
 		if(gdb_server) gdb_server->logger_import >> *logger->logger_export[logger_index++];*/
-		linux_os->logger_import >> *logger->logger_export[logger_index++];
+//		linux_os->logger_import >> *logger->logger_export[logger_index++];
 /*		for(unsigned int i = 0; i < MAX_BUS_TRANSACTION_SPY; i++)
 			if(bus_msg_spy[i] != NULL)
 				bus_msg_spy[i]->logger_import >> *logger->logger_export[logger_index++];
