fbecfa3aa972ed8c577983f88aa7b6b60f99c386
gracia <gracia@ea533459-3c0a-0410-a024-a90cf4b01ff5>
2009-07-09 10:43:31 +0000

    unisim_lib/unisim/component/tlm2/com/str7_gpio: 	+ Added skeleton for str7 GPIO component
    git-svn-id: https://unisim.org/svn/devel@11985 ea533459-3c0a-0410-a024-a90cf4b01ff5

---
+++ unisim_lib/unisim/component/tlm2/com/str7_gpio/str7_gpio.hh
@@ -0,0 +1,116 @@
+/*
+ *  Copyright (c) 2009,
+ *  Commissariat a l'Energie Atomique (CEA)
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification,
+ *  are permitted provided that the following conditions are met:
+ *
+ *   - Redistributions of source code must retain the above copyright notice, this
+ *     list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of CEA nor the names of its contributors may be used to
+ *     endorse or promote products derived from this software without specific prior
+ *     written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED.
+ *  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ *  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ *  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ *  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Authors: Daniel Gracia Perez (daniel.gracia-perez@cea.fr)
+ */
+
+#ifndef __UNISIM_COMPONENT_TLM2_COM_STR7_GPIO_STR7_GPIO_HH__
+#define __UNISIM_COMPONENT_TLM2_COM_STR7_GPIO_STR7_GPIO_HH__
+
+#include <systemc.h>
+#include <tlm.h>
+#include <tlm_utils/passthrough_target_socket.h>
+#include <tlm_utils/simple_initiator_socket.h>
+#include <inttypes.h>
+
+#include "unisim/kernel/service/service.hh"
+#include "unisim/kernel/logger/logger.hh"
+
+namespace unisim {
+namespace component {
+namespace tlm2 {
+namespace com {
+namespace str7_gpio {
+	
+	template <unsigned int BUS_WIDTH = 32>
+	class STR7_GPIO :
+	public sc_module,
+	public unisim::kernel::service::Object,
+	public tlm::tlm_fw_transport_if<>
+	{
+public:
+	/* input socket for memory transactions */
+	tlm::tlm_target_socket<BUS_WIDTH> in_mem;
+	
+	STR7_GPIO(const sc_module_name& name, Object* parent = 0);
+	virtual ~STR7_GPIO();
+	
+	virtual bool Setup();
+	
+private:
+	typedef STR7_GPIO<BUS_WIDTH> THIS_MODULE;
+	
+	/* START: GPIO registers and methods */
+	uint16_t pc0;
+	uint16_t pc1;
+	uint16_t pc2;
+	uint16_t pd;
+	unisim::kernel::service::Register<uint16_t> reg_pc0;
+	unisim::kernel::service::Register<uint16_t> reg_pc1;
+	unisim::kernel::service::Register<uint16_t> reg_pc2;
+	unisim::kernel::service::Register<uint16_t> reg_pd;
+	uint16_t ReadRegister(uint64_t address, bool update = true);
+	void WriteRegister(uint64_t address, uint16_t value, bool update = true);
+	/* END: GPIO registers and methods */
+	
+	/* Base address that will be used by the GPIO and its parameter to set it up */
+	uint64_t base_address;
+	unisim::kernel::service::Parameter<uint64_t> param_base_address;
+	
+	/* START: methods implementing the "in_mem" socket */
+	virtual void b_transport(tlm::tlm_generic_payload& trans, sc_core::sc_time& t);
+	virtual tlm::tlm_sync_enum nb_transport_fw(tlm::tlm_generic_payload& trans, tlm::tlm_phase& phase, sc_core::sc_time& t);
+	virtual bool get_direct_mem_ptr(tlm::tlm_generic_payload& trans, tlm::tlm_dmi& dmi_data);
+	virtual unsigned int transport_dbg(tlm::tlm_generic_payload& trans);
+	/* END: methods implementing the "in_mem" socket */
+
+	/* START: verbose options */
+	bool verbose_all;
+	unisim::kernel::service::Parameter<bool> param_verbose_all;
+	bool verbose_setup;
+	unisim::kernel::service::Parameter<bool> param_verbose_setup;
+	bool VerboseSetup();
+	bool verbose_tlm;
+	unisim::kernel::service::Parameter<bool> param_verbose_tlm;
+	bool VerboseTLM();
+	/* END: verbose options */
+	
+	/* the logger */
+	unisim::kernel::logger::Logger logger;
+};
+	
+} // end of namespace str7_gpio
+} // end of namespace com
+} // end of namespace tlm2
+} // end of namespace component
+} // end of namespace unisim
+
+#endif // __UNISIM_COMPONENT_TLM2_COM_STR7_GPIO_STR7_GPIO_HH__
