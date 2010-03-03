efd1b75f423a54e24c32f74ba819fc3aefe4f5dc
mouchard <mouchard@ea533459-3c0a-0410-a024-a90cf4b01ff5>
2010-03-03 17:37:26 +0000

    UNISIM Docs: -Improved web site (theme switch, increased maximum vertical size of items)
    git-svn-id: https://unisim.org/svn/devel@13130 ea533459-3c0a-0410-a024-a90cf4b01ff5

--- unisim_docs/www/build.sh
+++ unisim_docs/www/build.sh
@@ -1,42 +1,74 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-	STYLE=softy
-else
-	STYLE=$1
-fi
+DEFAULT_THEME=softy
 
 rm -rf site
 mkdir -p site
 mkdir -p site/images
-mkdir -p site/themes
 
-THEMES=`cd themes; find . \( -name "*.css" -o -name "*.png" \)`
+THEMES=`cd themes; ls --color=never`
 
 for THEME in ${THEMES}; do
-	if [ -f "themes/${THEME}" ]; then
-		echo "Copying themes/${THEME}"
-		mkdir -p site/themes/`dirname "${THEME}"`
-		cp "themes/${THEME}" "site/themes/${THEME}"
+	if [ -d "themes/${THEME}" ]; then
+		THEME_NAV=
+		for THEME2 in ${THEMES}; do
+			if [ "${THEME}" = "${DEFAULT_THEME}" ]; then
+				if [ "${THEME2}" = "${DEFAULT_THEME}" ]; then
+					THEME_NAV="${THEME_NAV} <a href=\"index.html\">${THEME2}</a>"
+				else
+					THEME_NAV="${THEME_NAV} <a href=\"${THEME2}/index.html\">${THEME2}</a>"
+				fi
+			else
+				if [ "${THEME2}" = "${DEFAULT_THEME}" ]; then
+					THEME_NAV="${THEME_NAV} <a href=\"../index.html\">${THEME2}</a>"
+				else
+					THEME_NAV="${THEME_NAV} <a href=\"../${THEME2}/index.html\">${THEME2}</a>"
+				fi
+			fi
+		done
+
+		if [ "${THEME}" = "${DEFAULT_THEME}" ]; then
+			ROOT=
+		else
+			ROOT="${THEME}"
+			mkdir -p site/${THEME}
+		fi
+
+		mkdir -p site/${ROOT}/theme
+		mkdir -p site/${ROOT}/images
+
+		echo "Building stylesheet for theme ${THEME}"
+		cpp -P -Itemplate -Ithemes/${THEME} template/template.css > site/${ROOT}/theme/style.css || exit 1
+		cp themes/${THEME}/*.png site/${ROOT}/theme
+
+
+		CONTENTS=`cd content; find . -name "*.html"`
+
+		for CONTENT in ${CONTENTS}; do
+			CONTENT_DIR=`dirname ${CONTENT}`
+			echo "Building ${CONTENT_DIR}.html for theme ${THEME}..."
+			cpp "-DTHEME_NAV=${THEME_NAV}" -P -Itemplate -Icontent/${CONTENT_DIR} template/template.html > site/${ROOT}/${CONTENT_DIR}.html || exit -1
+		done
+
+		IMAGES=`cd images; find . -name "*.png"`
+
+		for IMAGE in ${IMAGES}; do
+			if [ -f "images/${IMAGE}" ]; then
+				echo "Copying images/${IMAGE} for theme ${THEME}"
+				mkdir -p site/${THEME}/images/`dirname "${IMAGE}"`
+				cp "images/${IMAGE}" "site/${ROOT}/images/${IMAGE}"
+			fi
+		done
 	fi
 done
 
-IMAGES=`cd images; find . -name "*.png"`
 
-for IMAGE in ${IMAGES}; do
-	if [ -f "images/${IMAGE}" ]; then
-		echo "Copying images/${IMAGE}"
-		mkdir -p site/images/`dirname "${IMAGE}"`
-		cp "images/${IMAGE}" "site/images/${IMAGE}"
-	fi
-done
-
-CONTENTS=`cd content; find . -name "*.html"`
-
-for CONTENT in ${CONTENTS}; do
-	CONTENT_DIR=`dirname ${CONTENT}`
-	echo "Building ${CONTENT_DIR}.html..."
-	cpp -DSTYLESHEET=\"themes/${STYLE}/style.css\" -P -Itemplate -Icontent/${CONTENT_DIR} template/template.html > site/${CONTENT_DIR}.html || exit -1
-done
+# CONTENTS=`cd content; find . -name "*.html"`
+# 
+# for CONTENT in ${CONTENTS}; do
+# 	CONTENT_DIR=`dirname ${CONTENT}`
+# 	echo "Building ${CONTENT_DIR}.html..."
+# 	cpp -DSTYLESHEET=\"themes/${STYLE}/style.css\" -P -Itemplate -Icontent/${CONTENT_DIR} template/template.html > site/${CONTENT_DIR}.html || exit -1
+# done
 
 echo "The web site is in ${PWD}/site"
