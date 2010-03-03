ccdf7ce398b38b53aae63922095e3af9b58ae2cb
mouchard <mouchard@ea533459-3c0a-0410-a024-a90cf4b01ff5>
2010-03-03 15:45:14 +0000

    UNISIM Docs: -Improved web site
    git-svn-id: https://unisim.org/svn/devel@13128 ea533459-3c0a-0410-a024-a90cf4b01ff5

--- unisim_docs/www/build.sh
+++ unisim_docs/www/build.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
+
+if [ -z "$1" ]; then
+	STYLE=softy
+else
+	STYLE=$1
+fi
+
 rm -rf site
 mkdir -p site
 mkdir -p site/images
 mkdir -p site/themes
 
-THEMES=`cd themes; find . -name "*.css"`
+THEMES=`cd themes; find . \( -name "*.css" -o -name "*.png" \)`
 
 for THEME in ${THEMES}; do
 	if [ -f "themes/${THEME}" ]; then
-		echo "Copying ${THEME}"
+		echo "Copying themes/${THEME}"
 		mkdir -p site/themes/`dirname "${THEME}"`
 		cp "themes/${THEME}" "site/themes/${THEME}"
 	fi
@@ -18,7 +25,7 @@ IMAGES=`cd images; find . -name "*.png"`
 
 for IMAGE in ${IMAGES}; do
 	if [ -f "images/${IMAGE}" ]; then
-		echo "Copying ${IMAGE}"
+		echo "Copying images/${IMAGE}"
 		mkdir -p site/images/`dirname "${IMAGE}"`
 		cp "images/${IMAGE}" "site/images/${IMAGE}"
 	fi
@@ -29,7 +36,7 @@ CONTENTS=`cd content; find . -name "*.html"`
 for CONTENT in ${CONTENTS}; do
 	CONTENT_DIR=`dirname ${CONTENT}`
 	echo "Building ${CONTENT_DIR}.html..."
-	cpp -P -Itemplate -Icontent/${CONTENT_DIR} template/template.html > site/${CONTENT_DIR}.html || exit -1
+	cpp -DSTYLESHEET=\"themes/${STYLE}/style.css\" -P -Itemplate -Icontent/${CONTENT_DIR} template/template.html > site/${CONTENT_DIR}.html || exit -1
 done
 
 echo "The web site is in ${PWD}/site"
