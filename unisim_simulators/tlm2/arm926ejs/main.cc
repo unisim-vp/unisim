781b45a82574507a14742a63bb7261931331ae1b
Daniel Gracia Perez <daniel.gracia-perez@cea.fr>
2010-04-07 13:28:21 +0200

    Removed useless python code and options.
    Former-commit-id: 5c36a6e57d347753ff35123d091e311a374b0e94

--- unisim_simulators/tlm2/arm926ejs/main.cc
+++ unisim_simulators/tlm2/arm926ejs/main.cc
@@ -32,7 +32,6 @@
  * Authors: Daniel Gracia Perez (daniel.gracia-perez@cea.fr)
  */
 
-// #include <Python.h>
 #include <iostream>
 #include <sstream>
 #include <list>
@@ -155,30 +154,7 @@ void help(char *prog_name) {
 	cerr << " --no-run" << endl;
 	cerr << " -n" << endl;
 	cerr << "            do not launch the simulator, only apply the options" << endl << endl;
-	cerr << " --python-debugger" << endl;
-	cerr << " -p" << endl;
-	cerr << "            run in python mode" << endl;
 }
-
-// void LaunchPython()
-// {
-// 	int argc = 1;
-// 	wchar_t *argv[3];
-// 	const char *p1 = "python";
-// 	const char *p2 = "-i";
-// 	argv[0] = (wchar_t *)malloc(1000 * sizeof(wchar_t));
-// 	argv[1] = 0; // (wchar_t *)malloc(1000 * sizeof(wchar_t));
-// 	argv[2] = 0;
-// 	mbsrtowcs(argv[0], &p1, 1000, 0);
-// 	mbsrtowcs(argv[1], &p2, 1000, 0);
-// 	
-// 	Py_Initialize();
-// 	PyRun_SimpleString("from time import time,ctime\n"
-// 					   "print('Today is', ctime(time()))\n");
-// 	Py_Main(argc, argv);
-// 	Py_Finalize();
-// 	
-// }
 
 void ShowVersion()
 {
