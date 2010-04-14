f9daf632cdb7c97e6f648379bf8294cfb64477f7
Gilles Mouchard <gilles.mouchard@cea.fr>
2010-04-14 15:12:55 +0200

    Packaging: -Added support of shared data to ppcemu and embedded-ppc-g4-board simulators -Added some icon to ppcemu and embedded-ppc-g4-board simulators
    Former-commit-id: 9aa1f068d2fdd3ccef7040518163393ba742caba

---
+++ package/dist_package.sh
@@ -0,0 +1,549 @@
+#!/bin/bash
+HERE=`pwd`
+MY_DIR=`dirname $0`
+if test ${MY_DIR} = "."; then
+	MY_DIR=${HERE}
+elif test ${MY_DIR} = ".."; then
+	MY_DIR=${HERE}/..
+fi
+
+#NUM_PROCESSORS=`cat /proc/cpuinfo | cut -f 1 | grep vendor_id | wc -l`
+
+case `uname` in
+	MINGW*)
+		EXE_SUFFIX=".exe"
+		;;
+	*)
+		EXE_SUFFIX=
+		;;
+esac
+
+function usage
+{
+	echo "Usage: `basename $0` <OS> <genisslib version> <ppcemu version> <ppcemu-system version> <embedded-ppc-g4-board version> <systemc> <systemc-sdl> <tlm2.0>"
+	echo "OS: deb | rpm | mingw"
+}
+
+function BuildRPM
+{
+	NAME="$1"
+	shift
+	VERSION="$1"
+	shift
+	RELEASE="$1"
+	shift
+	URL="$1"
+	shift
+	LICENSE="$1"
+	shift
+	SUMMARY="$1"
+	shift
+	DESCRIPTION="$1"
+	shift
+	GROUP="$1"
+	shift
+	MAINTAINER="$1"
+	shift
+	FILES=`printf "$1" | sed -e 's/ /\n/g'`
+	shift
+	SYSTEM_FILES=`printf "$1" | sed -e 's/ /\n/g'`
+	shift
+	START_ICON="$1"
+	shift
+	START_PROGRAM="$1"
+	shift
+	START_PARAMS="$1"
+	shift
+
+	TOP_DIR="${HOME}/tmp/rpm"
+
+	if [ -d "${TOP_DIR}" ]; then
+		echo "ERROR! Directory ${TOP_DIR} already exists. Please remove ${TOP_DIR} before."
+		exit -1
+	fi
+
+	SPEC="${TOP_DIR}/SPECS/${NAME}.spec"
+
+	mkdir -p ${TOP_DIR}/{BUILD,RPMS/{i586,noarch,x86_64},SOURCES,SRPMS,SPECS,tmp} 
+	cp -f "${NAME}-${VERSION}.tar.gz" "${TOP_DIR}/SOURCES"
+
+	echo "%define _topdir     ${HOME}/tmp/rpm" > "${SPEC}"
+	echo "%define name        ${NAME}" >> "${SPEC}"
+	echo "%define release     ${RELEASE}" >> "${SPEC}"
+	echo "%define version     ${VERSION}" >> "${SPEC}"
+	echo "%define buildroot %{_topdir}/%{name}-%{version}-root" >> "${SPEC}"
+
+	echo "BuildRoot: %{buildroot}" >> "${SPEC}"
+	echo "Summary:   ${SUMMARY}" >> "${SPEC}"
+	echo "License:   ${LICENSE}" >> "${SPEC}"
+	echo "Name:      %{name}" >> "${SPEC}"
+	echo "Version:   %{version}" >> "${SPEC}"
+	echo "Release:   %{release}" >> "${SPEC}"
+	echo "Source:    %{name}-%{version}.tar.gz" >> "${SPEC}"
+	echo "Group:     ${GROUP}" >> "${SPEC}"
+	echo "URL:       ${URL}" >> "${SPEC}"
+
+	echo "%description" >> "${SPEC}"
+	echo "${DESCRIPTION}" >> "${SPEC}"
+
+	echo "%prep" >> "${SPEC}"
+	echo "%setup -q" >> "${SPEC}"
+
+
+	echo "%build" >> "${SPEC}"
+	printf "./configure " >> "${SPEC}"
+	for arg in "${@}"; do
+		printf "\'${arg}\' " >> "${SPEC}"
+	done
+	printf "\n" >> "${SPEC}"
+	echo "%make" >> "${SPEC}"
+
+	echo "%install" >> "${SPEC}"
+	echo "echo \"prefix is: %{_prefix}\"" >> "${SPEC}"
+	echo "make install prefix=\$RPM_BUILD_ROOT%{_prefix}" >> "${SPEC}"
+
+	echo "%files" >> "${SPEC}"
+	echo "%defattr(-,root,root)" >> "${SPEC}"
+	for FILE in ${FILES}; do
+		echo "%{_prefix}/${FILE}" >> "${SPEC}"
+	done
+
+	rpmbuild -v -bb --clean "${SPEC}" || exit -1
+
+	mv -f ${TOP_DIR}/RPMS/*/${NAME}-${VERSION}-${RELEASE}.*.rpm "${HERE}"
+
+	rm -rf "${TOP_DIR}"
+}
+
+function find_deb_dependencies
+{
+	local PKG_DEPS=""
+	local TMP=""
+	for FILE in $@; do
+		if [ -f ${FILE} ] && [ -x ${FILE} ]; then
+			local FILE_HEAD=`head -c 4 ${FILE}`
+			local ELF_HEAD=`echo -e "\0177ELF"`
+			if [ "${FILE_HEAD}" = "${ELF_HEAD}" ]; then
+				local SO_DEPS=`ldd ${FILE} | sed -e 's/^.*=>//' -e 's/^.*not a dynamic executable//' | cut -f 1 -d "("`
+				#echo "SO_DEPS=\"${SO_DEPS}\""
+				for SO_DEP in ${SO_DEPS}; do
+					local PKGS=`dpkg -S ${SO_DEP} 2> /dev/null | cut -f 1 -d ":"`
+					#echo "PKGS=\"${PKGS}\""
+					for PKG in ${PKGS}; do
+						local PKG_VERSION=`dpkg -s ${PKG} 2> /dev/null | grep Version | cut -f 2- -d ":" | sed 's/^[ \t]*//'`
+						local regex="([0-9][0-9]*:)*[0-9][0-9]*(\.[0-9][0-9]*)*"
+						if [[ ${PKG_VERSION} =~ ${regex} ]]; then
+							PKG_VERSION=$BASH_REMATCH
+						fi
+						if [ -n "${TMP}" ]; then
+							TMP+="\n"
+						fi
+						TMP+="${PKG} (>= ${PKG_VERSION})"
+					done
+				done
+			fi
+		fi
+	done
+	
+	local PKG_DEPS=`echo -e "${TMP}" | sort -u | sed ':a;N;$!ba;s/\n/, /g'`
+	echo ${PKG_DEPS}
+}
+
+function BuildDEB
+{
+	NAME="$1"
+	shift
+	VERSION="$1"
+	shift
+	RELEASE="$1"
+	shift
+	URL="$1"
+	shift
+	LICENSE="$1"
+	shift
+	SUMMARY="$1"
+	shift
+	DESCRIPTION="$1"
+	shift
+	GROUP="$1"
+	shift
+	MAINTAINER="$1"
+	shift
+	FILES=`printf "$1" | sed -e 's/ /\n/g'`
+	shift
+	SYSTEM_FILES=`printf "$1" | sed -e 's/ /\n/g'`
+	shift
+	START_ICON="$1"
+	shift
+	START_PROGRAM="$1"
+	shift
+	START_PARAMS="$1"
+	shift
+
+	PREFIX=/usr
+	TOP_DIR="${HOME}/tmp/deb"
+
+	if [ -d "${TOP_DIR}" ]; then
+		echo "ERROR! Directory ${TOP_DIR} already exists. Please remove ${TOP_DIR} before."
+		exit -1
+	fi
+
+	mkdir -p ${TOP_DIR}/{BUILD,INSTALL,SOURCES} 
+	cp -f "${NAME}-${VERSION}.tar.gz" "${TOP_DIR}/SOURCES"
+
+	SOURCES_DIR="${TOP_DIR}/SOURCES"
+	BUILD_DIR="${TOP_DIR}/BUILD"
+	INSTALL_DIR="${TOP_DIR}/INSTALL/${NAME}-${VERSION}"
+
+	mkdir -p ${BUILD_DIR}
+
+	# uncompress source
+	cd ${BUILD_DIR}
+	tar zxvf "${SOURCES_DIR}/${NAME}-${VERSION}.tar.gz" || exit -1
+
+	# configure
+	cd ${BUILD_DIR}/${NAME}-${VERSION}
+	./configure --prefix=${PREFIX} "${@}" || exit -1
+	
+	# build
+	NUM_PROCESSORS=`cat /proc/cpuinfo | cut -f 1 | grep vendor_id | wc -l`
+	make -j ${NUM_PROCESSORS} || exit -1
+
+	# install
+	fakeroot make install prefix=${INSTALL_DIR}${PREFIX} || exit -1
+
+	local CONTROL_FILE="${INSTALL_DIR}/DEBIAN/control"
+	local PREINST_FILE="${INSTALL_DIR}/DEBIAN/preinst"
+	local TEMPLATES_FILE="${INSTALL_DIR}/DEBIAN/templates"
+	local MD5SUMS_FILE="${INSTALL_DIR}/DEBIAN/md5sums"
+
+	# compute the installed size
+	local INSTALLED_SIZE=0
+
+	# create DEBIAN dubdirectory
+	mkdir -p ${INSTALL_DIR}/DEBIAN
+	
+	# Fill-in DEBIAN/md5sums
+	printf "Computing md5 sums..."
+	cd "${INSTALL_DIR}"
+	for FILE in ${FILES}
+	do
+		if [ -f "${INSTALL_DIR}${PREFIX}/${FILE}" ]; then
+			md5sum ".${PREFIX}/${FILE}" | sed 's/\.\///g' >> ${MD5SUMS_FILE}
+			INSTALLED_SIZE=$((${INSTALLED_SIZE} + `stat -c%s "${INSTALL_DIR}${PREFIX}/${FILE}"`))
+		fi
+	done
+	printf "Done\n"
+	echo "Installed size is $(((${INSTALLED_SIZE} + 1023) /1024)) KB"
+
+	# Finding package dependencies
+	printf "Searching dependencies..."
+	local DEPS=`find_deb_dependencies ${file_list}`
+	printf "Done\n"
+	
+	ARCH=i386
+	# Fill-in DEBIAN/control
+	echo "Package: ${NAME}" > ${CONTROL_FILE}
+	echo "Version: ${VERSION}" >> ${CONTROL_FILE}
+	echo "Section: ${SECTION}" >> ${CONTROL_FILE}
+	echo "Priority: optional" >> ${CONTROL_FILE}
+	echo "Architecture: ${ARCH}" >> ${CONTROL_FILE}
+	echo "Depends: ${DEPS}" >> ${CONTROL_FILE}
+	echo "Installed-Size: $(((${INSTALLED_SIZE} + 1023) /1024))" >> ${CONTROL_FILE}
+	echo "Maintainer: ${MAINTAINER}" >> ${CONTROL_FILE}
+	echo "Description: ${DESCRIPTION}" >> ${CONTROL_FILE}
+	echo "" >> ${CONTROL_FILE}
+
+	echo "========================================="
+	echo "=            DEBIAN/md5sums             ="
+	echo "========================================="
+	cat ${MD5SUMS_FILE}
+	
+	echo "========================================="
+	echo "=            DEBIAN/control             ="
+	echo "========================================="
+	cat ${CONTROL_FILE}
+
+	# Build the package
+	echo "========================================="
+	echo "=            Package build              ="
+	echo "========================================="
+	
+	cd ${HERE}
+	echo "Building ${NAME}_${VERSION}_${ARCH}.deb"
+	fakeroot dpkg-deb --build "${INSTALL_DIR}" "${HERE}" &> /dev/null || exit -1
+
+	rm -rf "${TOP_DIR}"
+}
+
+function BuildWinInstaller
+{
+	NAME="$1"
+	shift
+	VERSION="$1"
+	shift
+	RELEASE="$1"
+	shift
+	URL="$1"
+	shift
+	LICENSE="$1"
+	shift
+	SUMMARY="$1"
+	shift
+	DESCRIPTION="$1"
+	shift
+	GROUP="$1"
+	shift
+	MAINTAINER="$1"
+	shift
+	FILES="$1"
+	shift
+	SYSTEM_FILES=`printf "$1" | sed -e 's/ /\n/g'`
+	shift
+	START_ICON="$1"
+	shift
+	START_PROGRAM="$1"
+	shift
+	START_PARAMS="$1"
+	shift
+
+	PREFIX=/
+	TOP_DIR="${HOME}/tmp/mingw"
+
+	if [ -d "${TOP_DIR}" ]; then
+		echo "ERROR! Directory ${TOP_DIR} already exists. Please remove ${TOP_DIR} before."
+		exit -1
+	fi
+
+	mkdir -p ${TOP_DIR}/{BUILD,INSTALL,SOURCES} 
+	cp -f "${NAME}-${VERSION}.tar.gz" "${TOP_DIR}/SOURCES"
+
+	SOURCES_DIR="${TOP_DIR}/SOURCES"
+	BUILD_DIR="${TOP_DIR}/BUILD"
+	INSTALL_DIR="${TOP_DIR}/INSTALL/${NAME}-${VERSION}"
+
+	mkdir -p ${BUILD_DIR}
+
+	# uncompress source
+	cd ${BUILD_DIR}
+	tar zxvf "${SOURCES_DIR}/${NAME}-${VERSION}.tar.gz" || exit -1
+
+	# configure
+	cd ${BUILD_DIR}/${NAME}-${VERSION}
+	./configure --prefix=${PREFIX} "${@}" || exit -1
+	
+	# build
+	make || exit -1
+
+	# install
+	make install prefix=${INSTALL_DIR}${PREFIX} || exit -1
+
+	ISS_FILENAME=${INSTALL_DIR}/${NAME}-${VERSION}.iss
+	
+	# Fill-in dist.iss
+	echo "[Setup]" > ${ISS_FILENAME}
+	echo "AppName=${NAME}" >> ${ISS_FILENAME}
+	echo "AppVerName=${NAME}-${VERSION}" >> ${ISS_FILENAME}
+	echo "AppPublisher=CEA" >> ${ISS_FILENAME}
+	echo "AppPublisherURL=${URL}" >> ${ISS_FILENAME}
+	echo "AppSupportURL=${URL}" >> ${ISS_FILENAME}
+	echo "AppUpdatesURL=${URL}" >> ${ISS_FILENAME}
+	echo "DefaultDirName={sd}\\${NAME}-${VERSION}" >> ${ISS_FILENAME}
+	echo "DefaultGroupName=${NAME}-${VERSION}" >> ${ISS_FILENAME}
+	echo "AllowNoIcons=yes" >> ${ISS_FILENAME}
+	echo "OutputDir=dist" >> ${ISS_FILENAME}
+	echo "OutputBaseFilename=setup-${NAME}-${VERSION}" >> ${ISS_FILENAME}
+	echo "Compression=lzma" >> ${ISS_FILENAME}
+	echo "SolidCompression=yes" >> ${ISS_FILENAME}
+	echo "" >> ${ISS_FILENAME}
+	echo "[Languages]" >> ${ISS_FILENAME}
+	echo "Name: \"english\"; MessagesFile: \"compiler:Default.isl\"" >> ${ISS_FILENAME}
+	echo "" >> ${ISS_FILENAME}
+	echo "[Tasks]" >> ${ISS_FILENAME}
+	if ! [ -z "${START}" ] && ! [ -z "${START_ICON}" ]; then
+		echo "Name: \"desktopicon\"; Description: \"{cm:CreateDesktopIcon}\"; GroupDescription: \"{cm:AdditionalIcons}\"; Flags: unchecked" >> ${ISS_FILENAME}
+		echo "Name: \"quicklaunchicon\"; Description: \"{cm:CreateQuickLaunchIcon}\"; GroupDescription: \"{cm:AdditionalIcons}\"; Flags: unchecked" >> ${ISS_FILENAME}
+	fi
+	echo "" >> ${ISS_FILENAME}
+	echo "[Files]" >> ${ISS_FILENAME}
+	cd "${INSTALL_DIR}"
+
+	for SYSTEM_FILE in ${SYSTEM_FILES}; do
+		mkdir -p ${INSTALL_DIR}/bin
+		cp -f ${SYSTEM_FILE} ${INSTALL_DIR}/bin
+		FILES="${FILES} bin/`basename ${SYSTEM_FILE}`"
+	done
+
+	for FILE in ${FILES}
+	do
+		if [ -f "${INSTALL_DIR}/${FILE}" ]; then
+			STRIPPED_FILENAME=`echo ${FILE} | sed 's/\.\///g'`
+			echo "Source: \"${STRIPPED_FILENAME}\"; DestDir: \"{app}/`dirname ${STRIPPED_FILENAME}`\"; Flags: ignoreversion" >> ${ISS_FILENAME}
+		fi
+	done
+	cd "${HERE}"
+
+	if ! [ -z "${START}" ] && ! [ -z "${START_ICON}" ]; then
+		echo "" >> ${ISS_FILENAME}
+		echo "[Icons]" >> ${ISS_FILENAME}
+		echo "Name: \"{group}\\${NAME}\"; Filename: \"{app}\\${START}\"; IconFilename: \"{app}\\${START_ICON}\"; Parameters: \"${START_PARAMS}\"" >> ${ISS_FILENAME}
+		echo "Name: \"{commondesktop}\\${NAME}\"; Filename: \"{app}\\${START}\"; IconFilename: \"{app}\\${START_ICON}\"; Parameters: \"${START_PARAMS}\"; Tasks: desktopicon" >> ${ISS_FILENAME}
+		echo "Name: \"{userappdata}\\${NAME}\"; Filename: \"{app}\\${START}\"; IconFilename: \"{app}\\${START_ICON}\"; Tasks: quicklaunchicon" >> ${ISS_FILENAME}
+	fi
+	
+	echo "========================================="
+	echo "=            Inno Setup Script          ="
+	echo "========================================="
+	cat ${ISS_FILENAME}
+	
+	# Build the package
+	echo "========================================="
+	echo "=            Package build              ="
+	echo "========================================="
+
+	/c/Program\ Files/Inno\ Setup\ 5/ISCC.exe ${ISS_FILENAME} || exit -1
+	cp -f ${INSTALL_DIR}/dist/setup-${NAME}-${VERSION}.exe ${HERE}
+
+	rm -rf "${TOP_DIR}"
+}
+
+function BuildPackage
+{
+	local OS="$1"
+	shift
+
+	case ${OS} in
+		deb)
+			BuildDEB "${@}"
+			;;
+		rpm)
+			BuildRPM "${@}"
+			;;
+		mingw)
+			BuildWinInstaller "${@}"
+			;;
+		*)
+			usage
+			exit -1
+			;;
+	esac
+}
+
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] || [ -z "$5" ] || [ -z "$6" ] || [ -z "$7" ] || [ -z "$8" ]; then
+	usage
+	exit -1
+fi
+
+OS="$1"
+GENISSLIB_VERSION="$2"
+PPCEMU_VERSION="$3"
+PPCEMU_SYSTEM_VERSION="$4"
+EMBEDDED_PPC_G4_BOARD_VERSION="$5"
+SYSTEMC="$6"
+SYSTEMC_SDL="$7"
+TLM20="$8"
+
+# BuildPackage \
+# 	"${OS}" \
+# 	"genisslib" \
+# 	"${GENISSLIB_VERSION}" \
+# 	"1" \
+# 	"http://www.unisim-vp.com" \
+# 	"GPL" \
+# 	"UNISIM genisslib" \
+# 	"UNISIM Instruction set simulator generator" \
+# 	"Development/Tools" \
+# 	"Gilles Mouchard <gilles.mouchard@cea.fr>" \
+# 	"bin/genisslib${EXE_SUFFIX}" \
+#     "/bin/libgcc_s_dw2-1.dll" \
+# 	"" \
+# 	"" \
+# 	"" \
+# 	"CXXFLAGS=-O3 -g"
+
+BuildPackage \
+	"${OS}" \
+	"ppcemu" \
+	"${PPCEMU_VERSION}" \
+	"1" \
+	"http://www.unisim-vp.com" \
+	"BSD" \
+	"UNISIM ppcemu" \
+	"UNISIM ppcemu is user level PowerPC simulator with support of ELF32 binaries and Linux system call translation." \
+	"Development/Tools" \
+	"Gilles Mouchard <gilles.mouchard@cea.fr>" \
+	"bin/ppcemu${EXE_SUFFIX} \
+	bin/ppcemu-debug${EXE_SUFFIX} \
+	share/ppcemu/AUTHORS \
+	share/ppcemu/COPYING \
+	share/ppcemu/ChangeLog \
+	share/ppcemu/INSTALL \
+	share/ppcemu/NEWS \
+	share/ppcemu/README \
+	share/ppcemu/gdb_powerpc.xml" \
+    "/bin/libgcc_s_dw2-1.dll /bin/libxml2-2.dll" \
+	"share/ppcemu/unisim.ico" \
+	"bin/ppcemu${EXE_SUFFIX}" \
+	"-c %prefix%share/ppcemu/sim_config.xml" \
+	"--with-systemc=${SYSTEMC}" \
+	"CXXFLAGS=-O3 -g"
+
+exit
+
+BuildPackage \
+	"${OS}" \
+	"ppcemu_system" \
+	"${PPCEMU_SYSTEM_VERSION}" \
+	"1" \
+	"http://www.unisim-vp.com" \
+	"BSD" \
+	"UNISIM ppcemu-system" \
+	"UNISIM ppcemu-system is a full system simulator of a \"PowerMac G4 PCI\" like machine (MPC7447A/MPC107) with Linux boot support." \
+	"Development/Tools" \
+	"Gilles Mouchard <gilles.mouchard@cea.fr>" \
+	"bin/ppcemu-system${EXE_SUFFIX} \
+	bin/ppcemu-system-debug${EXE_SUFFIX} \
+	share/ppcemu-system/AUTHORS \
+	share/ppcemu-system/COPYING \
+	share/ppcemu-system/ChangeLog \
+	share/ppcemu-system/INSTALL \
+	share/ppcemu-system/NEWS \
+	share/ppcemu-system/README \
+	share/ppcemu-system/gdb_powerpc.xml \
+	share/ppcemu-system/device_tree_pmac_g4.xml \
+	share/ppcemu-system/pc_linux_fr_keymap.xml \
+	share/ppcemu-system/pc_linux_us_keymap.xml \
+	share/ppcemu-system/pc_windows_keymap.xml" \
+    "/bin/libgcc_s_dw2-1.dll /bin/libxml2-2.dll /bin/SDL.dll" \
+	"share/ppcemu-system/unisim.ico" \
+	"bin/ppcemu-system${EXE_SUFFIX}" \
+	"-c %prefix%share/ppcemu-system/sim_config.xml" \
+	"--with-systemc=${SYSTEMC_SDL}" \
+	"CXXFLAGS=-O3 -g"
+
+BuildPackage \
+	"${OS}" \
+	"embedded_ppc_g4_board" \
+	"${EMBEDDED_PPC_G4_BOARD_VERSION}" \
+	"1" \
+	"http://www.unisim-vp.com" \
+	"BSD" \
+	"UNISIM embedded-ppc-g4-board" \
+	"UNISIM embedded-ppc-g4-board is a full system simulator of an MPC7447A/MPC107 board with support of ELF32 binaries and targeted for industrial applications." \
+	"Development/Tools" \
+	"Gilles Mouchard <gilles.mouchard@cea.fr>" \
+	"bin/embedded-ppc-g4-board${EXE_SUFFIX} \
+	bin/embedded-ppc-g4-board-debug${EXE_SUFFIX} \
+	share/embedded-ppc-g4-board/AUTHORS \
+	share/embedded-ppc-g4-board/COPYING \
+	share/embedded-ppc-g4-board/ChangeLog \
+	share/embedded-ppc-g4-board/INSTALL \
+	share/embedded-ppc-g4-board/NEWS \
+	share/embedded-ppc-g4-board/README \
+	share/embedded-ppc-g4-board/gdb_powerpc.xml" \
+    "/bin/libgcc_s_dw2-1.dll /bin/libxml2-2.dll" \
+	"share/embedded-ppc-g4-board/unisim.ico" \
+	"bin/embedded-ppc-g4-board${EXE_SUFFIX}" \
+	"-c %prefix%share/embedded-ppc-g4-board/sim_config.xml" \
+	"--with-systemc=${SYSTEMC}" \
+	"CXXFLAGS=-O3 -g"
