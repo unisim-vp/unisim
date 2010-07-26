d39d893370a739d07e81362517f215e4dc4a37f4
Yves Lhuillier <lhuillier@localhost.localdomain>
2010-07-26 16:01:02 +0200

    constness bugfix
    Former-commit-id: 6f8d77f1c80ee115fd48f9fc820578d1e1ada416

--- unisim_tools/genisslib/ciscgenerator.cc
+++ unisim_tools/genisslib/ciscgenerator.cc
@@ -354,7 +354,7 @@ CiscGenerator::codetype_decl( Product_t& _product ) const {
   _product.code( " uint8_t                   str[capacity];\n" );
   _product.code( " enum Exception_t { NotEnoughBytes };\n" );
   _product.code( " CodeType() : size( 0 ) { for (int idx = capacity; (--idx) >= 0;) str[idx] = 0; };\n" );
-  _product.code( " CodeType( uint8_t* src, unsigned int sz )\n" );
+  _product.code( " CodeType( uint8_t const* src, unsigned int sz )\n" );
   _product.code( " : size( std::min( sz, capacity*8 ) )\n{\n" );
   _product.code( "   for (int idx = capacity; (--idx) >= 0;) str[idx] = 0;\n" );
   _product.code( "   for (int idx = (size+7)/8; (--idx) >= 0;) str[idx] = src[idx];\n" );
