495aac22eb230c820d0867ff168a046e41941047
Daniel Gracia Perez <daniel.gracia-perez@cea.fr>
2010-10-06 10:46:51 +0200

    [Fix] Fixed armemu cache initialization.
    Former-commit-id: eb30a1bf36c0c597533ead9edbc02b2a5b518204

--- unisim_lib/unisim/component/cxx/processor/arm/armemu/cache.cc
+++ unisim_lib/unisim/component/cxx/processor/arm/armemu/cache.cc
@@ -62,313 +62,334 @@ namespace processor {
 namespace arm {
 namespace armemu {
 
-	Cache::Cache() :
-	rand(1, -1, rand.Max, rand.Min),
-	m_is_ok(false),
-	m_size(0),
-	m_round_robin_replacement_policy(false)
+Cache::Cache() :
+rand(1, -1, rand.Max, rand.Min),
+m_is_ok(false),
+m_size(0),
+m_round_robin_replacement_policy(false)
+{
+	for (uint32_t set = 0; set < m_sets_; set++)
 	{
-		for (uint32_t set = 0; set < m_sets_; set++)
+		for (uint32_t way = 0; way < m_associativity_; way++)
 		{
-			for (uint32_t way = 0; way < m_associativity_; way++)
-			{
-				m_tag[set][way] = 0;
-				memset(m_data[set][way], 0, m_line_size_);
-				m_valid[set][way] = false;
-				m_dirty[set][way] = false;
-			}
-			m_replacement_history[set] = 0;
+			m_tag[set][way] = 0;
+			memset(m_data[set][way], 0, m_line_size_);
+			m_valid[set][way] = false;
+			m_dirty[set][way] = false;
 		}
+		m_replacement_history[set] = 0;
 	}
+}
 
-	Cache::~Cache()
-	{
-		// nothing to do
-	}
+Cache::~Cache()
+{
+	// nothing to do
+}
 
-	bool 
-	Cache::
-	SetSize(uint32_t size)
+bool 
+Cache::
+SetSize(uint32_t size)
+{
+	m_is_ok = false;
+	// test that the size parameter is a valid value
+	if ( size == 0 )
+		m_is_ok = true;
+	else if ( size > 0 )
 	{
-		m_is_ok = false;
-		// test that the size parameter is a valid value
-		if ( size == 0 )
-			m_is_ok = true;
-		else if ( size > 0 )
-		{
-			// ARM926EJS caches can only be between 4KB (0x01000) and 128KB (0x020000)
-			//   and must be 2^x = size
+		// ARM926EJS caches can only be between 4KB (0x01000) and 128KB (0x020000)
+		//   and must be 2^x = size
 #ifdef ARMEMU_CACHE_DEBUG
-			cerr << "Cache size = 0x" << hex << size << dec
-					<< "(" << (unsigned int)size << ")" << endl;
+		cerr << "Cache size = 0x" << hex << size << dec
+				<< "(" << (unsigned int)size << ")" << endl;
 #endif
-			if ( (size & ~(uint32_t)0x03f000) == 0 )
-			{
-				uint32_t i = size;
-				uint32_t count = 0;
+		m_is_ok = true;
+		switch ( size )
+		{
+			case 0x01000: // 4KB
+				m_size = size;
+				m_set_mask = (uint32_t)0x01f << 5;
+				m_tag_mask = ~(m_set_mask | (uint32_t)0x01f);
+				m_tag_shift = 10;
+				break;
+			case 0x02000: // 8KB
+				m_size = size;
+				m_set_mask = (uint32_t)0x03f << 5;
+				m_tag_mask = ~(m_set_mask | (uint32_t)0x01f);
+				m_tag_shift = 11;
+				break;
+			case 0x04000: // 16KB
+				m_size = size;
+				m_set_mask = (uint32_t)0x07f << 5;
+				m_tag_mask = ~(m_set_mask | (uint32_t)0x01f);
+				m_tag_shift = 12;
+				break;
+			case 0x08000: // 32KB
+				m_size = size;
+				m_set_mask = (uint32_t)0x0ff << 5;
+				m_tag_mask = ~(m_set_mask | (uint32_t)0x01f);
+				m_tag_shift = 13;
+				break;
+			case 0x010000: // 64KB
+				m_size = size;
+				m_set_mask = (uint32_t)0x01ff << 5;
+				m_tag_mask = ~(m_set_mask | (uint32_t)0x01f);
+				m_tag_shift = 14;
+				break;
+			case 0x020000: // 128KB
+				m_size = size;
+				m_set_mask = (uint32_t)0x03ff << 5;
+				m_tag_mask = ~(m_set_mask | (uint32_t)0x01f);
+				m_tag_shift = 15;
+				break;
+			default:
+				// given size is not supported
+				m_is_ok = false;
+				break;
+		}
+
 #ifdef ARMEMU_CACHE_DEBUG
-				cerr << "Cache size is in range" << endl;
+		if ( m_is_ok )
+		{	
+			std::cerr << "m_set_mask   = 0x" << std::hex << m_set_mask << std::dec << std::endl;
+			std::cerr << "m_set_shift_ = " << m_set_shift_ << std::endl;
+			std::cerr << "m_tag_mask   = 0x" << std::hex << m_tag_mask << std::dec << std::endl;
+			std::cerr << "m_tag_shift  = " << m_tag_shift << std::endl;
+		}
 #endif
-				while ( i != 0 )
-				{
-					if ( i & (uint32_t)0x01 ) count++;
-					i = i >> 1;
-				}
-				if ( count == 1 )
-				{
-					m_size = size;
-					m_is_ok = true;
-				}
-			}
-			// compute the masks
-			if (m_is_ok)
-			{
-				m_set_mask = 1;
-				m_tag_shift = m_set_shift_ + 1;
-				for(uint32_t t_nsets = m_size / m_associativity_ / m_line_size_;
-					t_nsets != 0;
-					t_nsets = t_nsets >> 1)
-				{
-					m_set_mask = (m_set_mask << 1) + 1;
-					m_tag_shift++;
-				}
-				m_set_mask = m_set_mask << m_set_shift_;
-				m_tag_mask = ~(m_set_mask | m_index_mask_);
-			}
-		}
-
-		return m_is_ok;
 	}
 
-	uint32_t
-	Cache::
-	GetSize()
-	{
-		return m_size;
-	}
+	return m_is_ok;
+}
 
-	bool
-	Cache::
-	SetRRReplacementPolicy(bool round_robin)
-	{
-		bool prev = m_round_robin_replacement_policy;
-		m_round_robin_replacement_policy = round_robin;
-		return prev;
-	}
+uint32_t
+Cache::
+GetSize()
+{
+	return m_size;
+}
 
-	bool 
-	Cache::IsOK()
-	const
-	{
-		return m_is_ok;
-	}
+bool
+Cache::
+SetRRReplacementPolicy(bool round_robin)
+{
+	bool prev = m_round_robin_replacement_policy;
+	m_round_robin_replacement_policy = round_robin;
+	return prev;
+}
 
-	uint32_t
-	Cache::
-	GetTag(uint32_t addr)
-	const
-	{
-		return (addr & m_tag_mask) >> m_tag_shift;
-	}
+bool 
+Cache::IsOK()
+const
+{
+	return m_is_ok;
+}
 
-	void
-	Cache::
-	SetTag(uint32_t set, uint32_t way, uint32_t tag)
-	{
-		m_tag[set][way] = tag;
-	}
+uint32_t
+Cache::
+GetTag(uint32_t addr)
+const
+{
+	return (addr & m_tag_mask) >> m_tag_shift;
+}
+
+void
+Cache::
+SetTag(uint32_t set, uint32_t way, uint32_t tag)
+{
+	m_tag[set][way] = tag;
+}
 	
-	uint32_t
-	Cache::
-	GetSet(uint32_t addr)
-	const
-	{
-		return (addr & m_set_mask) >> m_set_shift_;
-	}
+uint32_t
+Cache::
+GetSet(uint32_t addr)
+const
+{
+	return (addr & m_set_mask) >> m_set_shift_;
+}
 	
-	uint32_t
-	Cache::
-	GetIndex(uint32_t addr)
-	const
-	{
-		return addr & m_index_mask_;
-	}
+uint32_t
+Cache::
+GetIndex(uint32_t addr)
+const
+{
+	return addr & m_index_mask_;
+}
 	
-	bool
-	Cache::
-	HasTag(uint32_t tag, uint32_t set)
-	const
-	{
-		uint32_t current_way = 0;
-		
-		while (current_way < m_associativity_)
-		{
-			if (m_tag[set][current_way] == tag)
-				return true;
-			current_way++;
-		}
-		return false;
-	}
-	
-	bool
-	Cache::
-	GetWay(uint32_t tag, uint32_t set, uint32_t *way)
-	const
-	{
-		bool found = false;
-		uint32_t current_way = 0;
-		
-		while (current_way < m_associativity_)
-		{
-			found = (m_tag[set][current_way] == tag);
-			if (found)
-			{
-				*way = current_way;
-				return found;
-			}
-			current_way++;
-		}
-		*way = 0;
-		return found;
-	}
+bool
+Cache::
+HasTag(uint32_t tag, uint32_t set)
+const
+{
+	uint32_t current_way = 0;
 
-	uint32_t
-	Cache::
-	GetNewWay(uint32_t set)
+	while (current_way < m_associativity_)
 	{
-		// apply the arm926ejs replacement policy
-		if ( m_round_robin_replacement_policy )
-		{
-			// current replacement policy is round robin
-			m_replacement_history[set] = (m_replacement_history[set] + 1) %
-					m_associativity_;
-		}
-		else
-		{
-			// current replacement policy is random
-			m_replacement_history[set] =
-					rand.Generate(m_associativity_/2) +
-					(m_associativity_/2);
-		}
-		return m_replacement_history[set];
+		if (m_tag[set][current_way] == tag)
+			return true;
+		current_way++;
 	}
+	return false;
+}
 
-	uint32_t
-	Cache::
-	GetBaseAddress(uint32_t set, uint32_t way)
-	const
+bool
+Cache::
+GetWay(uint32_t tag, uint32_t set, uint32_t *way)
+const
+{
+	bool found = false;
+	uint32_t current_way = 0;
+
+	while (current_way < m_associativity_)
 	{
-		uint32_t tag = m_tag[set][way];
-		uint32_t addr = 0;
-		addr = (tag << m_tag_shift) |
-				(set << m_set_shift_);
-		return addr;
+		found = (m_tag[set][current_way] == tag);
+		if (found)
+		{
+			*way = current_way;
+			return found;
+		}
+		current_way++;
 	}
+	*way = 0;
+	return found;
+}
 
-	uint32_t
-	Cache::
-	GetBaseAddressFromAddress(uint32_t addr)
-	const
+uint32_t
+Cache::
+GetNewWay(uint32_t set)
+{
+	// apply the arm926ejs replacement policy
+	if ( m_round_robin_replacement_policy )
 	{
+		// current replacement policy is round robin
+		m_replacement_history[set] = (m_replacement_history[set] + 1) %
+				m_associativity_;
+	}
+	else
+	{
+		// current replacement policy is random
+		m_replacement_history[set] =
+				rand.Generate(m_associativity_/2) +
+				(m_associativity_/2);
+	}
+	return m_replacement_history[set];
+}
+
+uint32_t
+Cache::
+GetBaseAddress(uint32_t set, uint32_t way)
+const
+{
+	uint32_t tag = m_tag[set][way];
+	uint32_t addr = 0;
+	addr = (tag << m_tag_shift) |
+			(set << m_set_shift_);
+	return addr;
+}
+
+uint32_t
+Cache::
+GetBaseAddressFromAddress(uint32_t addr)
+const
+{
 #ifdef ARMEMU_CACHE_DEBUG
-		cerr << "GettingAddressFromAddress" << endl
-				<< " - addr       = 0x" << hex << (unsigned long int)addr << endl
-				<< " - m_set_mask = 0x" << (unsigned long int)m_set_mask << dec
-				<< " (shift = " << (unsigned int)m_set_shift_ << ")" << endl
-				<< " - m_tag_mask = 0x" << hex << (unsigned long int)m_tag_mask << dec
-				<< " (shift = " << (unsigned int)m_tag_shift << ")" << endl
-				<< " - new_addr   = 0x" << hex
-				<< (unsigned long int)(addr & (m_set_mask | m_tag_mask)) << dec
-				<< endl;
+	cerr << "GettingAddressFromAddress" << endl
+			<< " - addr       = 0x" << hex << (unsigned long int)addr << endl
+			<< " - m_set_mask = 0x" << (unsigned long int)m_set_mask << dec
+			<< " (shift = " << (unsigned int)m_set_shift_ << ")" << endl
+			<< " - m_tag_mask = 0x" << hex << (unsigned long int)m_tag_mask << dec
+			<< " (shift = " << (unsigned int)m_tag_shift << ")" << endl
+			<< " - new_addr   = 0x" << hex
+			<< (unsigned long int)(addr & (m_set_mask | m_tag_mask)) << dec
+			<< endl;
 #endif
-		addr = addr & (m_set_mask | m_tag_mask);
-		return addr;
-	}
+	addr = addr & (m_set_mask | m_tag_mask);
+	return addr;
+}
 
-	uint32_t
-	Cache::
-	GetData(uint32_t set, uint32_t way, uint8_t **data)
-	{
-		*data = m_data[set][way];
-		return m_line_size_;
-	}
-	
-	uint32_t
-	Cache::
-	GetData(uint32_t set, uint32_t way, uint32_t index, uint32_t size, uint8_t **data)
-	{
-		uint32_t max_size = m_line_size_ - index;
-		*data = &(m_data[set][way][index]);
-		return (max_size < size) ? max_size : size;
-	}
-	
-	uint32_t
-	Cache::
-	GetDataCopy(uint32_t set, uint32_t way, uint8_t *data)
-	const
-	{
-		memcpy(data, m_data[set][way], m_line_size_);
-		return m_line_size_;
-	}
-	
-	uint32_t
-	Cache::
-	GetDataCopy(uint32_t set, uint32_t way, uint32_t index, uint32_t size, uint8_t *data)
-	const
-	{
-		uint32_t max_size = m_line_size_ - index;
-		size = (max_size < size) ? max_size : size;
-		memcpy(data, &(m_data[set][way][index]), size);
-		return size;
-	}
-	
-	uint32_t
-	Cache::
-	SetData(uint32_t set, uint32_t way, uint8_t *data)
-	{
-		memcpy(m_data[set][way], data, m_line_size_);
-		return m_line_size_;
-	}
+uint32_t
+Cache::
+GetData(uint32_t set, uint32_t way, uint8_t **data)
+{
+	*data = m_data[set][way];
+	return m_line_size_;
+}
 
-	uint32_t
-	Cache::
-	SetData(uint32_t set, uint32_t way, uint32_t index, uint32_t size, uint8_t *data)
-	{
-		uint32_t max_size = m_line_size_ - index;
-		size = (max_size < size) ? max_size : size;
-		memcpy(&(m_data[set][way][index]), data, size);
-		return size;
-	}
-	
-	uint8_t
-	Cache::
-	GetValid(uint32_t set, uint32_t way)
-	const
-	{
-		return m_valid[set][way];
-	}
+uint32_t
+Cache::
+GetData(uint32_t set, uint32_t way, uint32_t index, uint32_t size, uint8_t **data)
+{
+	uint32_t max_size = m_line_size_ - index;
+	*data = &(m_data[set][way][index]);
+	return (max_size < size) ? max_size : size;
+}
 
-	void
-	Cache::
-	SetValid(uint32_t set, uint32_t way, uint8_t valid)
-	{
-		m_valid[set][way] = valid;
-	}
-	
-	uint8_t
-	Cache::
-	GetDirty(uint32_t set, uint32_t way)
-	const
-	{
-		return m_dirty[set][way];
-	}
-	
-	void
-	Cache::
-	SetDirty(uint32_t set, uint32_t way, uint8_t dirty)
-	{
-		m_dirty[set][way] = dirty;
-	}
+uint32_t
+Cache::
+GetDataCopy(uint32_t set, uint32_t way, uint8_t *data)
+const
+{
+	memcpy(data, m_data[set][way], m_line_size_);
+	return m_line_size_;
+}
+
+uint32_t
+Cache::
+GetDataCopy(uint32_t set, uint32_t way, uint32_t index, uint32_t size, uint8_t *data)
+const
+{
+	uint32_t max_size = m_line_size_ - index;
+	size = (max_size < size) ? max_size : size;
+	memcpy(data, &(m_data[set][way][index]), size);
+	return size;
+}
+
+uint32_t
+Cache::
+SetData(uint32_t set, uint32_t way, uint8_t *data)
+{
+	memcpy(m_data[set][way], data, m_line_size_);
+	return m_line_size_;
+}
+
+uint32_t
+Cache::
+SetData(uint32_t set, uint32_t way, uint32_t index, uint32_t size, uint8_t *data)
+{
+	uint32_t max_size = m_line_size_ - index;
+	size = (max_size < size) ? max_size : size;
+	memcpy(&(m_data[set][way][index]), data, size);
+	return size;
+}
+
+uint8_t
+Cache::
+GetValid(uint32_t set, uint32_t way)
+const
+{
+	return m_valid[set][way];
+}
+
+void
+Cache::
+SetValid(uint32_t set, uint32_t way, uint8_t valid)
+{
+	m_valid[set][way] = valid;
+}
+
+uint8_t
+Cache::
+GetDirty(uint32_t set, uint32_t way)
+const
+{
+	return m_dirty[set][way];
+}
+
+void
+Cache::
+SetDirty(uint32_t set, uint32_t way, uint8_t dirty)
+{
+	m_dirty[set][way] = dirty;
+}
 
 } // end of namespace armemu
 } // end of namespace arm
