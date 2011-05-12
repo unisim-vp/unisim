a0d2e1f02e7452e8f624e93fc595ffb8a5397d60
Daniel Gracia Perez <daniel.gracia-perez@cea.fr>
2011-05-13 00:06:05 +0200

    [Dev] UAPI: Updated demo code to use dlopen.
    Former-commit-id: 1043605f5432e5bab4d52822f7de149d6ec8fcd9

--- unisim_simulators/tlm2/arm926ejs/api_test.cc
+++ unisim_simulators/tlm2/arm926ejs/api_test.cc
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <iostream>
 #include <sstream>
+#include <dlfcn.h>
 #include "unisim/uapi/uapi.h"
+
+#if 0
 
 std::string VariableString(UniVariable var)
 {
@@ -40,43 +43,168 @@ std::string VariableString(UniVariable var)
 
 	return str.str();
 }
+#endif
 
-int main()
+void *simlib;
+
+// simulator methods
+typedef UnisimSimulator (* _usCreateSimulator_type)(char *, char **);
+_usCreateSimulator_type _usCreateSimulator;
+typedef bool (* _usDestroySimulator_type)(UnisimSimulator);
+_usDestroySimulator_type _usDestroySimulator;
+typedef UnisimSimulatorStatus (* _usSimulatorGetStatus_type)(UnisimSimulator);
+_usSimulatorGetStatus_type _usSimulatorGetStatus;
+typedef UnisimSimulatorSetupStatus (*_usSimulatorSetup_type)(UnisimSimulator);
+_usSimulatorSetup_type _usSimulatorSetup;
+typedef const char *(*_usSimulatorGetVersion_type)(UnisimSimulator);
+_usSimulatorGetVersion_type _usSimulatorGetVersion;
+typedef bool (* _usSimulatorRun_type)(UnisimSimulator);
+_usSimulatorRun_type _usSimulatorRun;
+typedef void (* _usSimulatorStop_type)(UnisimSimulator);
+_usSimulatorStop_type _usSimulatorStop;
+typedef UnisimVariable (* _usSimulatorGetVariable_type)(UnisimSimulator, const char *);
+_usSimulatorGetVariable_type _usSimulatorGetVariable;
+typedef UnisimVariable (* _usSimulatorGetVariableWithType_type)(UnisimSimulator, const char *, UnisimVariableType);
+_usSimulatorGetVariableWithType_type _usSimulatorGetVariableWithType;
+typedef UnisimVariable *(* _usSimulatorGetVariableList_type)(UnisimSimulator, const char *);
+_usSimulatorGetVariableList_type _usSimulatorGetVariableList;
+typedef UnisimVariable *(* _usSimulatorGetVariableListWithType_type)(UnisimSimulator, const char *, UnisimVariableType);
+_usSimulatorGetVariableListWithType_type _usSimulatorGetVariableListWithType;
+
+// variable methods
+typedef const char *(* _usVariableGetName_type)(UnisimVariable);
+_usVariableGetName_type _usVariableGetName;
+typedef UnisimVariableType (* _usVariableGetType_type)(UnisimVariable);
+_usVariableGetType_type _usVariableGetType;
+typedef bool (* _usVariableVisible_type)(UnisimVariable);
+_usVariableVisible_type _usVariableVisible;
+typedef bool (* _usVariableMutable_type)(UnisimVariable);
+_usVariableMutable_type _usVariableMutable;
+typedef bool (* _usVariableSerializable_type)(UnisimVariable);
+_usVariableSerializable_type _usVariableSerializable;
+typedef const char *(* _usVariableGetValueAsString_type)(UnisimVariable);
+_usVariableGetValueAsString_type _usVariableGetValueAsString;
+typedef void (* _usDestroyVariable_type)(UnisimVariable);
+_usDestroyVariable_type _usDestroyVariable;
+
+template <typename T>
+bool load_sym(void *lib, const char *name, T &func)
 {
-	UniSimulator sim;
+	void *_func = dlsym(lib, name);
+	if ( _func == NULL )
+	{ 
+		std::cerr << "Could not get '" << name << "'" << std::endl;
+		return false;
+	}
+	func = (T)_func;
+
+	return true;
+}
+
+bool close_lib()
+{
+	if ( dlclose(simlib) != 0 )
+	{
+		std::cerr << "Error while closing dynamic library" << std::endl;
+		return false;
+	}
+	return true;
+}
+
+bool load_lib()
+{
+	simlib = dlopen("/Users/gracia/Developer/unisim/arm926ejs/build/lib/liblibuapi.dylib", RTLD_LAZY);
+	if(simlib == NULL)
+	{
+		std::cerr << "Could not load simulator dynamic library"
+			<< std::endl;
+		return false;
+	}
+
+	if ( !load_sym(simlib, "usCreateSimulator", _usCreateSimulator) )
+		return false;
+	if ( !load_sym(simlib, "usDestroySimulator", _usDestroySimulator) )
+		return false;
+	if ( !load_sym(simlib, "usSimulatorGetStatus", _usSimulatorGetStatus) )
+		return false;
+	if ( !load_sym(simlib, "usSimulatorSetup", _usSimulatorSetup) )
+		return false;
+	if ( !load_sym(simlib, "usSimulatorGetVersion", _usSimulatorGetVersion) )
+		return false;
+	if ( !load_sym(simlib, "usSimulatorRun", _usSimulatorRun) )
+		return false;
+	if ( !load_sym(simlib, "usSimulatorStop", _usSimulatorStop) )
+		return false;
+	if ( !load_sym(simlib, "usSimulatorGetVariable", _usSimulatorGetVariable) )
+		return false;
+	if ( !load_sym(simlib, "usSimulatorGetVariableWithType", _usSimulatorGetVariableWithType) )
+		return false;
+	if ( !load_sym(simlib, "usSimulatorGetVariableList", _usSimulatorGetVariableList) )
+		return false;
+	if ( !load_sym(simlib, "usSimulatorGetVariableListWithType", _usSimulatorGetVariableListWithType) )
+		return false;
+
+	if ( !load_sym(simlib, "usVariableGetName", _usVariableGetName) )
+		return false;
+	if ( !load_sym(simlib, "usVariableGetType", _usVariableGetType) )
+		return false;
+	if ( !load_sym(simlib, "usVariableVisible", _usVariableVisible) )
+		return false;
+	if ( !load_sym(simlib, "usVariableMutable", _usVariableMutable) )
+		return false;
+	if ( !load_sym(simlib, "usVariableSerializable", _usVariableSerializable) )
+		return false;
+	if ( !load_sym(simlib, "usVariableGetValueAsString", _usVariableGetValueAsString) )
+		return false;
+	if ( !load_sym(simlib, "usDestroyVariable", _usDestroyVariable) )
+		return false;
+
+	return true;
+}
+
+int test()
+{
+	if ( !load_lib() )
+	{
+		std::cerr << "Could not load symbols" << std::endl;
+		return -1;
+	}
+
+	UnisimSimulator simulator;
 
 	// creating the simulator
-	sim = CreateSimulator(0, 0);
-	if ( sim == 0 ) 
+	simulator = _usCreateSimulator(0, 0);
+	if ( simulator == 0 ) 
 	{
 		std::cerr << "Could not create simulator" << std::endl;
-		return 0;
+		return -1;
 	}
 
 	// obtaining its version
-	const char *version = Version(sim);
-	if ( version == 0 )
+	const char *simulatorVersion = _usSimulatorGetVersion(simulator);
+	if ( simulatorVersion == 0 )
 		std::cerr << "Could not get the simulator version" << std::endl;
 	else
-		std::cerr << "Simulator version: " << version << std::endl;
+		std::cerr << "Simulator version: " << simulatorVersion << std::endl;
 
 	// getting a variable
-	UniVariable var;
+	UnisimVariable variable;
 	std::cerr << "Testing a single variable ('authors'):" << std::endl;
-	var = GetVariable(sim, "authors");
-	if ( var == 0 )
+	variable = _usSimulatorGetVariable(simulator, "authors");
+	if ( variable == 0 )
 		std::cerr << "Could not get the authors variable" << std::endl;
 	else
 	{
-		std::cerr << "The variable name is '" << GetName(var) << "'" <<  std::endl;
-		std::cerr << "Variable properties: " << std::endl
-			<< " - visible      = " << IsVisible(var) << std::endl
-			<< " - mutable      = " << IsMutable(var) << std::endl
-			<< " - serializable = " << IsSerializable(var) << std::endl;
-		std::cerr << "The variable value is '" << GetValueAsString(var) << "'" << std::endl;
+		std::cerr << "The variableiable name is '" << _usVariableGetName(variable) << "'" <<  std::endl;
+		std::cerr << "variableiable properties: " << std::endl
+			<< " - visible      = " << _usVariableVisible(variable) << std::endl
+			<< " - mutable      = " << _usVariableMutable(variable) << std::endl
+			<< " - serializable = " << _usVariableSerializable(variable) << std::endl;
+		std::cerr << "The variable value is '" << _usVariableGetValueAsString(variable) << "'" << std::endl;
 	}
-	DestroyVariable(var);
+	_usDestroyVariable(variable);
 
+#if 0
 	// getting multiple variables
 	UniVariable *var_list;
 	std::cerr << "Testing multiple variables:" << std::endl;
@@ -199,9 +327,28 @@ int main()
 		<< " non typed variables (from a total of "
 		<< num_vars << " variables)" << std::endl;
 
+#endif 
+
 	// destroying the simulator
-	bool st = DestroySimulator(sim);
-	if ( !st )
+	bool done = _usDestroySimulator(simulator);
+	if ( !done )
 		std::cerr << "Could not destroy simulator" << std::endl;
+
+	if ( !close_lib() )
+	{
+		std::cerr << "Could not correctly close the library" << std::endl;
+		return -1;
+	}
+
 	return 0;
+}
+
+int main()
+{
+	if ( test() != 0 )
+	{
+		std::cerr << "Could not do more than once" << std::endl;
+		return 0;
+	}
+	return test();
 }
