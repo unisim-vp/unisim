45756a263bf198e7b2847efb67a7082f8500f205
Daniel Gracia Perez <daniel.gracia-perez@cea.fr>
2011-07-26 13:46:49 +0200

    [Dev] ARMEMU: fixed mibench run script to use absolut paths.
    Former-commit-id: 2f6e9d7feeee26f2820d6ac5f3023e27215c0fe4

--- unisim_simulators/tlm2/armemu/share/test/mibench/run.py
+++ unisim_simulators/tlm2/armemu/share/test/mibench/run.py
@@ -1,6 +1,7 @@
 import shlex
 import subprocess
 import os
+import os.path
 import shutil
 import filecmp
 import time
@@ -119,9 +120,9 @@ if not options.config_path:
 	parser.print_help()
 	parser.error("missing directory containing the simulator configuration for each benchmark")
 
-benchmarks_path = options.benchmarks_path
-simulator = options.simulator
-config_path = options.config_path
+benchmarks_path = os.path(options.benchmarks_path)
+simulator = os.path(options.simulator)
+config_path = os.path(options.config_path)
 xunit = XUnit("mibench", time.time())
 automotive_basicmath()
 xunit.write()
