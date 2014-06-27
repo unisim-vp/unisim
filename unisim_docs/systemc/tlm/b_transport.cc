5bb2886268f6506e9bfbc704c42a459c681f206b
Gilles Mouchard <gilles.mouchard@cea.fr>
2014-06-27 17:14:47 +0200

    Updated SystemC tutorial. Added SystemC materials.
    Former-commit-id: dd62c490a6cc6a0fdd8fc32907a4e7eeddf6027e

--- unisim_docs/systemc/tlm/b_transport.cc
+++ unisim_docs/systemc/tlm/b_transport.cc
@@ -3,7 +3,7 @@ namespace tlm {
 template <typename TRANS = tlm_generic_payload>
 class tlm_blocking_transport_if : public virtual sc_core::sc_interface {
 public:
-	virtual void b_transport(TRANS& trans) = 0;
+	virtual void b_transport(TRANS& trans, sc_core::sc_time& t) = 0;
 };
 
 } // namespace tlm
--- unisim_docs/systemc/tlm/nb_transport.cc
+++ unisim_docs/systemc/tlm/nb_transport.cc
@@ -2,8 +2,8 @@ namespace tlm {
 
 enum tlm_phase { BEGIN_REQ, END_REQ, 
 	BEGIN_RESP, END_RESP };
-enum tlm_sync_enum { TLM_REJECTED = 0, TLM_ACCEPTED = 1, 
-	TLM_UPDATED = 2, TLM_COMPLETED = 3 };
+enum tlm_sync_enum { TLM_ACCEPTED = 0, 
+	TLM_UPDATED = 1, TLM_COMPLETED = 2 };
 
 template <typename TRANS = tlm_generic_payload, typename PHASE = tlm_phase>
 class tlm_nonblocking_transport_if : public virtual sc_core::sc_interface {
