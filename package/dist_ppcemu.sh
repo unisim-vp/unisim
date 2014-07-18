f2c051f88135b623d7e381ed20e3da0c6b49e113
Gilles Mouchard <gilles.mouchard@cea.fr>
2014-07-18 12:41:52 +0200

    ppcemu: -Merged with other branches for Linux OS upgrade
    Former-commit-id: 5e1e68d9768024878719ee0b34b1327f0b6d737f

--- package/dist_ppcemu.sh
+++ package/dist_ppcemu.sh
@@ -278,6 +278,7 @@ unisim/util/queue/queue.hh \
 unisim/util/simfloat/floating.hh \
 unisim/util/simfloat/integer.hh \
 unisim/util/simfloat/host_floating.hh \
+unisim/util/ieee754/ieee754.hh \
 unisim/util/device/register.hh \
 unisim/util/random/random.hh \
 unisim/util/allocator/allocator.hh \
@@ -295,6 +296,7 @@ unisim/util/os/linux_os/environment.hh \
 unisim/util/os/linux_os/files_flags.hh \
 unisim/util/os/linux_os/linux.hh \
 unisim/util/os/linux_os/ppc.hh \
+unisim/util/os/linux_os/errno.hh \
 unisim/util/dictionary/dictionary.hh \
 unisim/util/lexer/lexer.hh \
 unisim/util/parser/parser.hh \
