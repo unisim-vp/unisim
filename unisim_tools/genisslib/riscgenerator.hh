85088c242967b1c78946a75e1b360ba1431420d0
Yves Lhuillier <yves.lhuillier@gmail.com>
2014-08-12 23:14:23 +0200

    implicit/explicit specialization mechanisms improvment
    Former-commit-id: 9c625935dea36910ad0d0ada08c59f26ee252895

--- unisim_tools/genisslib/riscgenerator.hh
+++ unisim_tools/genisslib/riscgenerator.hh
@@ -30,11 +30,11 @@ struct RiscOpCode_t : public OpCode_t
   unsigned int                m_size;
   bool                        m_vlen;
     
-  RiscOpCode_t()
-    : m_mask( 0 ), m_bits( 0 ), m_size( 0 ), m_vlen( false )
-  {}
-  RiscOpCode_t( uint64_t mask, uint64_t bits, unsigned int size, bool vlen )
-    : m_mask( mask ), m_bits( bits ), m_size( size ), m_vlen( vlen )
+  // RiscOpCode_t()
+  //   : OpCode_t( ConstStr_t() ), m_mask( 0 ), m_bits( 0 ), m_size( 0 ), m_vlen( false )
+  // {}
+  RiscOpCode_t( ConstStr_t _symbol, uint64_t mask, uint64_t bits, unsigned int size, bool vlen )
+    : OpCode_t( _symbol ), m_mask( mask ), m_bits( bits ), m_size( size ), m_vlen( vlen )
   {}
     
   // Topology methods
@@ -43,10 +43,8 @@ struct RiscOpCode_t : public OpCode_t
   std::ostream&               details( std::ostream& _sink ) const;
 };
 
-struct RiscGenerator : public Generator {
-  typedef std::map<Operation_t const*,RiscOpCode_t> OpCodes_t;
-  
-  OpCodes_t                     m_opcodes;
+struct RiscGenerator : public Generator
+{
   unsigned int                  m_insn_ctypesize;
   ConstStr_t                    m_insn_ctype;
   ConstStr_t                    m_insn_cpostfix;
@@ -54,10 +52,10 @@ struct RiscGenerator : public Generator {
   RiscGenerator();
   ~RiscGenerator() {};
   
-  /* Risc specific Utilities */
-  RiscOpCode_t const&           opcode( Operation_t const* _op ) const;
-  RiscOpCode_t&                 opcode( Operation_t const* _op );
+  RiscOpCode_t const&           riscopcode( Operation_t const* _op ) const { return dynamic_cast<RiscOpCode_t const&>( opcode( _op ) ); }
+  RiscOpCode_t&                 riscopcode( Operation_t const* _op ) { return dynamic_cast<RiscOpCode_t&>( opcode( _op ) ); };
   
+  /* Risc specific Utilities */
   void                          finalize();
   void                          codetype_decl( Product_t& _product ) const;
   void                          codetype_impl( Product_t& _product ) const {}
