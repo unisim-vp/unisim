6951344325bbacd1b376cbaa45bf33f95e796230
Reda NOUACER <reda.nouacer@cea.fr>
2014-12-08 14:51:01 +0100

    remove redandante source file "coff_loader.tcc"
    Former-commit-id: dc41634a240f78cee0ba8962a3630eb78be21107

--- package/dist_star12x.sh
+++ package/dist_star12x.sh
@@ -365,7 +365,6 @@ unisim/util/debug/elf_symtab/elf_symtab.tcc \
 unisim/util/loader/elf_loader/elf_loader.tcc \
 unisim/util/loader/coff_loader/coff_loader.tcc \
 unisim/util/debug/coff_symtab/coff_symtab.tcc \
-unisim/util/loader/coff_loader/coff_loader.tcc \
 unisim/util/loader/coff_loader/ti/ti.tcc \
 unisim/service/debug/gdb_server/gdb_server.tcc \
 unisim/service/loader/elf_loader/elf_loader.tcc \
