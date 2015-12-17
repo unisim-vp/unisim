aa075d7b2a71fbde719ae9c9d3cd7cc1cd49b91a
Yves Lhuillier <yves.lhuillier@cea.fr>
2015-12-17 15:43:32 +0100

    Unisim Intel Linux emulation not ready, keeping old eulation file in simulator directory
    Former-commit-id: 1694ad2ad7ef6bf7ab082d3f27733fa96451f706

--- unisim_lib/unisim/component/cxx/processor/intel/toto.sh
+++
@@ -1,7 +0,0 @@
-mv isbranch.hh isa/branch.hh
-mv isfloatingpoint.hh isa/floatingpoint.hh
-mv isinteger.hh isa/integer.hh
-mv ismove.hh isa/move.hh
-mv issimd.hh isa/simd.hh
-mv isspecial.hh isa/special.hh
-mv isstring.hh isa/string.hh
