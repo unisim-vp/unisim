a5b26b418f1a95b2fb2173ad0c7b1ab449a7e1e1
Yves Lhuillier <yves.lhuillier@cea.fr>
2016-06-03 15:33:28 +0200

    ...
    Former-commit-id: 3907199f2f202af7e7cd0316c9e20a4466a0a626

--- unisim_lib/unisim/component/tlm2/processor/arm/cortex_r5f/cpu.cc
+++ unisim_lib/unisim/component/tlm2/processor/arm/cortex_r5f/cpu.cc
@@ -381,11 +381,15 @@ CPU::Run()
         unisim::component::cxx::processor::arm::F.Set( exceptions, not nFIQm );
         
         bool exception_taken = this->HandleAsynchronousException( exceptions ) != 0;
-        if (exception_taken and this->exception_trap_reporting_import)
-          this->exception_trap_reporting_import->ReportTrap(*this,"irq or fiq");
+        if (exception_taken) {
+          if (exception_trap_reporting_import)
+            exception_trap_reporting_import->ReportTrap(*this,"irq or fiq");
+          if (requires_finished_instruction_reporting and memory_access_reporting_import)
+            memory_access_reporting_import->ReportFinishedInstruction(this->current_insn_addr, this->next_insn_addr);
+        }
       }
     }
-  
+    
     if (unlikely(verbose_tlm))
     {
       PCPU::logger << DebugInfo
