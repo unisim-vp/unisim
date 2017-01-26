3de02a0d28ca9b08c860d42520c9d841c8716261
Yves Lhuillier <yves.lhuillier@cea.fr>
2017-01-26 14:19:15 +0100

    [AArch64] Cosmetic on ISA layout...
    Former-commit-id: f3d6690246f413854ddb31b49b8a95cd848127ea

--- unisim_lib/unisim/component/cxx/processor/arm/attic/arm64gen/modisa.cc
+++ unisim_lib/unisim/component/cxx/processor/arm/attic/arm64gen/modisa.cc
@@ -211,9 +211,80 @@ struct CMV
   
   bool comparable( CMV const& rhs ) const { return (rhs.compare_op == compare_op) and (rhs.is_reg == is_reg); }
   
-  bool operator < ( CMV const& rhs ) const { return order < rhs.order; }
+  bool operator < ( CMV const& rhs ) const
+  {
+    if (int delta = int(compare_op) - int(rhs.compare_op)) return (delta < 0);
+    if (int delta = int(rhs.is_reg) - int(is_reg)) return (delta < 0);
+    return order < rhs.order;
+  }
   
-  enum { EQ, GT, LT, GE, LE, HI, HS } compare_op;
+  char const*
+  GetElemType() const
+  {
+    switch (compare_op)
+      {
+      case EQ: case GT: case LT: case GE: case LE:
+        switch (ebytes)
+          {
+          case 1: return "S8";  break;
+          case 2: return "S16"; break;
+          case 4: return "S32"; break;
+          case 8: return "S64"; break;
+          default: throw 0;
+          }
+        break;
+      case HI: case HS:
+        switch (ebytes)
+          {
+          case 1: return "U8";  break;
+          case 2: return "U16"; break;
+          case 4: return "U32"; break;
+          case 8: return "U64"; break;
+          default: throw 0;
+          }
+        break;
+      }
+    return "??";
+  }
+  
+  std::string
+  GetOp2() const
+  {
+    if (not is_reg)
+      return "0";
+    std::ostringstream buf;
+    buf << "cpu.GetV" << GetElemType() << "(rm,e)";
+    return "0";
+  }
+  
+  std::string
+  GetCode() const
+  {
+    std::ostringstream buf;
+    buf << "\n";
+    buf << "  typedef typename ARCH::" << GetElemType() << " " << GetElemType() << ";\n\n";
+    buf << "  for (unsigned e=0; e < " << elements << "; e += 1) {\n";
+    
+    buf << "    " << GetElemType() << " op1( cpu.GetV" << GetElemType() << "(rn,e) ), op2( " << GetOp2() << " ), res;\n";
+    buf << "    res =  " << GetElemType() << "(not (op1 ";
+    switch (compare_op)
+      {
+      case EQ:          buf << "=="; break;
+      case LT:          buf << "<"; break;
+      case LE:          buf << "<="; break;
+      case GT: case HI: buf << ">"; break;
+      case GE: case HS: buf << ">="; break;
+      default: break;
+      }
+    buf << " op2)) - " << GetElemType() << "(1);\n";
+    buf << "    cpu.SetV" << GetElemType() << "(rd,e,res);\n";
+    buf << "  }\n";
+    buf << "  cpu.ClearHighV(rd," << (elements*ebytes) << ");\n";
+    
+    return buf.str();
+  }
+  
+  enum { EQ = 0, GE = 1, GT = 2, HI = 3, HS = 4, LE = 5, LT = 6 } compare_op;
   bool is_reg;
   int elements, ebytes, order;
 };
@@ -226,14 +297,13 @@ struct Order
   {
     std::string lhn(lhs->symbol.str()), rhn(rhs->symbol.str());
     CMV lhe, rhe;
-    if (not lhe.Decode(lhn) or not rhe.Decode(rhn) or not lhe.comparable( rhe ))
+    if (not lhe.Decode(lhn) or not rhe.Decode(rhn))
       return original[lhn] < original[rhn];
     return lhe < rhe;
   }
 };
 
 std::map<std::string,int> Order::original;
-
 
 void
 Generator::transform( Isa& source )
@@ -253,405 +323,35 @@ Generator::transform( Isa& source )
   std::sort( source.m_operations.begin(), source.m_operations.end(), Order() );
 }
 
+
+// void
+// Generator::transform( Isa& source )
 // {
 //   for (auto&& op : source.m_operations)
 //     {
 //       std::string opname = op->symbol.str();
       
-//       uintptr_t oplen = opname.size();
-//       if (oplen < 6)
-//         continue;
-//       bool isld;
-//       if      (opname.find("ld") == 0)
-//         {
-//           isld = true;
-//         }
-//       else if (opname.find("st") == 0)
-//         {
-//           isld = false;
-//         }
-//       else
+//       CMV cmv;
+//       if (not cmv.Decode(opname))
 //         continue;
       
-//       int selem = opname[2] - '0';
-//       if ((selem < 1) or (selem > 4))
-//         continue;
-      
-//       if (opname[3] != '_')
-//         continue;
-      
-//       int regcount = 0;
-//       switch (opname[4]) {
-//       case 's': regcount = 1; break;
-//       case 'd': regcount = 2; break;
-//       case 't': regcount = 3; break;
-//       case 'q': regcount = 4; break;
-//       default: break;
-//       }
-      
-//       if (regcount == 0)
-//         continue;
-      
-//       int rpt = 1;
-//       if (regcount != selem) {
-//         assert( selem == 1 );
-//         rpt = regcount;
-//       }
-      
-//       int elements, ebytes;
-//       enum { NoWBack, WBackImm, WBackReg } wback;
-//       bool half_vector = false;
-      
-//       {
-//         char etype;
-//         std::string addrmode;
-//         char* after;
-//         char const* ecp = &opname[5];
-//         elements = strtoul(ecp,&after,10);
-//         if (after == ecp)
-//           continue;
-//         switch (elements) {
-//         case 1: case 2: case 4: case 8:
-//           assert( oplen >= 7 );
-//           etype = opname[6];
-//           addrmode = opname.substr(7);
-//           break;
-//         case 16:
-//           assert( oplen >= 8 );
-//           etype = opname[7];
-//           addrmode = opname.substr(8);
-//           break;
-//         default:
-//           assert( false );
-//         }
-//         switch (etype) {
-//         case 'b': ebytes = 1; break;
-//         case 'h': ebytes = 2; break;
-//         case 's': ebytes = 4; break;
-//         case 'd': ebytes = 8; break;
-//         default:    assert( false );
-//         }
-        
-//         if (addrmode.size() == 0) {
-//           wback = NoWBack;
-//         } else if (addrmode == "_reg") {
-//           wback = WBackReg;
-//         } else if (addrmode == "_imm") {
-//           wback = WBackImm;
-//         } else {
-//           assert( false );
-//         }
-//         switch (ebytes*elements) {
-//         case 8: half_vector = true; break;
-//         case 16: half_vector = false; break;
-//         default: assert( false );
+//       Action* exec = 0;
+//       for (auto&& act : op->actions) {
+//         if (act->m_actionproto->m_symbol == "execute") {
+//           std::cerr << "WARNING: execute method already existing: overwriting.\n";
+//           exec = &*act;
 //         }
 //       }
       
-//       std::cerr << opname << DumpBitFields( op->bitfields ) << " (" << isld << selem << regcount << rpt << elements << ebytes << wback << half_vector << ")\n";
-      
-//       {
-//         /* Code Generation */
-//         std::stringstream buf;
-//         buf << "\n";
-//         buf << "  typedef typename ARCH::U64 U64;\n\n";
-//         buf << "  U64 addr( cpu.GetGSR(rn) );\n\n";
-//         buf << "  for (unsigned r = 0; r < " << (selem*rpt) << "; ++r) { cpu.UseVU" << (ebytes*8) << "((rt+r)%32," << elements << "); }\n\n";
-//         if (rpt > 1) {
-//           buf << "  for (unsigned r = 0; r < " << rpt << "; ++r) {\n";
-//           buf << "    for (unsigned e = 0; e < " << elements << "; ++e) {\n";
-//           std::string addrstr;
-//           {
-//             std::stringstream addrbuf;
-//             addrbuf << "addr + U64((r*" << elements << "+e)*" << ebytes << ")";
-//             addrstr = addrbuf.str();
-//           }
-//           if (isld)
-//             buf << "      cpu.SetVU" << (ebytes*8) << "((rt+r)%32, e, cpu.MemRead" << (ebytes*8) << "( " << addrstr << " ));\n";
-//           else
-//             buf << "      cpu.MemWrite32( " << addrstr << ", cpu.GetVU" << (ebytes*8) << "((rt+r)%32, e) );\n";
-//           buf << "    }\n";
-//           buf << "  }\n";
-//         } else /* (selem > 1) */ {
-//           buf << "  for (unsigned e = 0; e < " << elements << "; ++e) {\n";
-//           buf << "    for (unsigned s = 0; s < " << selem << "; ++s) {\n";
-//           std::string addrstr;
-//           {
-//             std::stringstream addrbuf;
-//             addrbuf << "addr + U64((e*" << selem << "+s)*" << ebytes << ")";
-//             addrstr = addrbuf.str();
-//           }
-//           if (isld)
-//             buf << "      cpu.SetVU" << (ebytes*8) << "((rt+s)%32, e, cpu.MemRead" << (ebytes*8) << "( " << addrstr << " ));\n";
-//           else
-//             buf << "      cpu.MemWrite32( " << addrstr << ", cpu.GetVU" << (ebytes*8) << "((rt+s)%32, e) );\n";
-//           buf << "    }\n";
-//           buf << "  }\n";
-//         }
-//         switch (wback) {
-//         case WBackImm: buf << "  cpu.SetGSR(rn, addr + U64(" << (elements*selem*ebytes) << ") );\n"; break;
-//         case WBackReg: buf << "  cpu.SetGSR(rn, addr + cpu.GetGZR(rm) );\n"; break;
-//         default: break;
-//         }
-        
-//         Action* exec = 0;
-//         for (auto&& act : op->actions) {
-//           if (act->m_actionproto->m_symbol == "execute") {
-//             std::cerr << "WARNING: execute method already existing: overwriting.\n";
-//             exec = &*act;
-//           }
-//         }
-      
-//         if (not exec) {
-//           ActionProto const* actionproto = source.actionproto( "execute" );
-//           exec = new Action( actionproto, new SourceCode("",FileLoc()), Vector<Comment>(), FileLoc() );
-//           op->add( exec );
-//         }
-      
-//         exec->m_source_code->content = buf.str();
+//       if (not exec) {
+//         ActionProto const* actionproto = source.actionproto( "execute" );
+//         exec = new Action( actionproto, new SourceCode("",FileLoc()), Vector<Comment>(), FileLoc() );
+//         op->add( exec );
 //       }
       
+//       exec->m_source_code->content = cmv.GetCode();
       
-//       // Action* exec = 0;
-//       // for (auto&& act : op->actions) {
-//       //   if (act->m_actionproto->m_symbol == "execute")
-//       //     exec = &*act;
-//       // }
-      
-//       // if (not exec) continue;
-      
-//       // std::string exec_code = exec->m_source_code->content.str();
-
-//       // bool x = query_replace( exec_code, "+ U64(imm)", "+ U64(S64(imm))" );
-//       // if (not x)
-//       //   continue;
-      
-//       // // std::cerr << "(" << isld << (rsz==64) << xsof << xoff << sext << __builtin_ctz( size ) << ")";
-//       // for (unsigned pad = opname.size(); pad < 12; ++pad)
-//       //   std::cerr << ' ';
-//       // std::cerr << opname << DumpBitFields( op->bitfields ) << "\n";
-      
-//       // exec->m_source_code->content = exec_code;
-      
-      
-//       // std::cerr << isld << std::endl;
-      
-//       // bool imm_is_signed = false;
-//       // for (auto const& bf : op->bitfields)
-//       // {
-//       //   if (OperandBitField const* opbf = dynamic_cast<OperandBitField const*>( &*bf )) {
-//       //     if ((opbf->symbol == "imm") and (opbf->sext)) {
-//       //       imm_is_signed = true;
-//       //       break;
-//       //     }
-//       //   }
-//       // }
-//       // if (not imm_is_signed)
-//       //   continue;
-      
-//       continue;
-      
-//       // {
-//       //   uintptr_t sep = opname.find("_");
-//       //   if ((sep == std::string::npos) or ((opname.size()-sep) != 5))
-//       //     continue;
-//       //   if (opname[sep+2] != 'x')
-//       //     continue;
-//       //   switch (opname[sep+1]) { case 'x': rsz =   64; break; case 'w': rsz =    32; break; default: throw 0; }
-//       //   switch (opname[sep+3]) { case 'x': xoff = true; break; case 'w': xoff = false; break; default: throw 0; }
-//       //   switch (opname[sep+4]) { case 's': xsof = true; break; case 'u': xsof = false; break; default: throw 0; }
-        
-//       //   uintptr_t pos = 3;
-//       //   if (opname[pos] == 's') { sext = true; pos += 1; }
-//       //   switch (opname[pos++]) {
-//       //   case 'b': size =  8; break;
-//       //   case 'h': size = 16; break;
-//       //   case 'w': size = 32; break;
-//       //   default: size = rsz; pos -= 1; break;
-//       //   }
-//       //   if (opname[pos] != '_') {
-//       //     std::cerr << "error while parsing: " << opname << "\n";
-//       //     throw 0;
-//       //   }
-//       // }
-      
-      
-//       // char sch = xsof ? 'S' : 'U';
-//       // unsigned tsz = xoff ? 64 : 32;
-      
-//       // std::set<std::string> types;
-//       // struct TpStr
-//       // {
-//       //   TpStr( char _sgn, int _sz ) { std::ostringstream buf; buf << _sgn << _sz; data[buf.str().copy( &data[0], 7 )] = '\0'; } char data[8];
-//       //   char const* get() const { return &data[0]; }
-//       // };
-      
-//       // std::string address;
-//       // { std::ostringstream buf; buf << "U64 offset( " << sch << "64(" << sch << tsz << "(cpu.GetGZR(rm))) << shift ), addr( cpu.GetGSR(rn) + offset );"; address = buf.str(); }
-//       // types.insert( "U64" );
-//       // types.insert( TpStr(sch,64).get() );
-//       // types.insert( TpStr(sch,tsz).get() );
-      
-//       // if (isld) {
-//       //   types.insert( TpStr('U',rsz).get() );
-//       //   if (sext) {
-//       //     types.insert( TpStr('S',rsz).get() );
-//       //     types.insert( TpStr('S',size).get() );
-//       //   }
-//       // } else {
-//       //   types.insert( TpStr('U',size).get() );
-//       // }
-
-//       // std::ostringstream buf;
-//       // buf << "\n";
-//       // for (auto&& tp : types)
-//       //   {
-//       //     buf << "  typedef typename ARCH::" << tp << " " << tp << ";\n";
-//       //   }
-//       // buf << "\n  " << address << "\n";
-      
-//       // if (isld)
-//       //   {
-//       //     buf << "  U" << rsz << " data( ";
-        
-//       //     std::string memread; { std::ostringstream mrbuf; mrbuf << "cpu.MemRead" << size << "(addr)"; memread = mrbuf.str(); }
-//       //     if (sext)  buf << "S" << rsz << "(S" << size << "(" << memread << "))";
-//       //     else       buf << memread;
-        
-//       //     buf << " );\n"
-//       //         << "  cpu.SetGZR(rt, data);\n";
-//       //   }
-//       // else
-//       //   {
-//       //     buf << "  U" << size <<  " data( cpu.GetGZR(rt) );\n"
-//       //         << "  cpu.MemWrite" << size << "(addr, data);\n";
-//       //   }
-      
-//       // Vector<Comment> com;
-          
-//       // ActionProto const* actionproto = source.actionproto( "execute" );
-          
-//       // op->add( new Action( actionproto, new SourceCode(buf.str(),FileLoc()), com, FileLoc() ) );
-      
-      
-//       // struct OpEnv { char const* ext; unsigned size; bool setstatus; };
-//       // std::vector<OpEnv> openvs{ {"_wsxtx", 32, false}, {"_xsxtx", 64, false}, {"s_wsxtx", 32, true}, {"s_xsxtx", 64, true} };
-      
-//       // struct OpCore { char const* name; char const* op; };
-//       // std::vector<OpCore> opcores{ {"add","+ "}, {"and", "& "}, {"bic", "& ~"}, {"sub", "- "},
-//       //                              {"eon", "^ ~"}, {"eor", "^ "}, {"orn", "| ~"}, {"orr", "| "} };
-      
-//       // for (auto&& openv : openvs) {
-//       //   for (auto&& opcore : opcores) {
-//       //     std::string on( opcore.name );
-//       //     on += openv.ext;
-//       //     if (on != opname)
-//       //       continue;
-//       //     std::cerr << opname << DumpBitFields( op->bitfields ) << '\n';
-          
-//       //     std::ostringstream oss;
-//       //     if (openv.size != srcsize)
-//       //       oss << "\n  typedef typename ARCH::S" << srcsize << " S" << srcsize << ";";
-//       //     oss
-//       //       << "\n  typedef typename ARCH::S" << openv.size << " S" << openv.size << ';'
-//       //       << "\n  typedef typename ARCH::U" << openv.size << " U" << openv.size << ';'
-//       //       << "\n  U" << openv.size << " op1( cpu.GetGZR(rn) ), op2( S" << openv.size << "(S" << srcsize << "(cpu.GetGZR(rm))) << imm ), res = op1 " << opcore.op << "op2;"
-//       //       << "\n  cpu.SetGZR(rd, res);";
-//       //     if (openv.setstatus)
-//       //       oss << "\n  // XXX: set status flags";
-//       //     oss << "\n";
-          
-//       //     Vector<Comment> com;
-          
-//       //     ActionProto const* actionproto = source.actionproto( "execute" );
-          
-//       //     // std::cerr << oss.str() << std::endl;
-          
-//       //     op->add( new Action( actionproto, new SourceCode(oss.str(),FileLoc()), com, FileLoc() ) );
-//       //     break;
-//       //   }
-//       // }
-      
-//       // bool x = query_replace( disasm_code, "DisasmGZXR(rd)", "DisasmGSXR(rd)" );
-//       // bool y = query_replace( disasm_code, "DisasmGZWR(rd)", "DisasmGSWR(rd)" );
-//       // query_replace( disasm_code, "  <<", " <<" );
-//       // assert( x^y );
-      
-//       // bool chk;
-      
-//       // chk = query_replace( disasm_code, "\n  sink << \"ands\\t\" << DisasmG", "" );
-//       // assert( chk );
-//       // char zs = disasm_code[0];
-//       // assert( (zs == 'Z') or (zs == 'S') );
-//       // char xw = disasm_code[1];
-//       // assert( toupper( opname[5] ) == xw );
-//       // disasm_code = disasm_code.substr( 2 );
-//       // chk = query_replace( disasm_code, "R(rd) << \", \"", "" );
-//       // assert( chk );
-      
-//       // disasm_code = std::string("\n  if (rd == 31)\n    sink << \"tst\\t\"") + disasm_code + "  else\n    sink << \"ands\\t\" << DisasmGZ" + xw + "R(rd) << \", \"" + disasm_code;
-        
-//       // std::cerr << disasm_code << std::endl;
-      
-//       // disasm.m_source_code->content = disasm_code.c_str();
-      
-//       // auto&& itr = op->bitfields.begin();
-      
-//       // if (opname.substr(opname.size()-5) != "xxror")
-//       //   continue;
-      
-//       // try {
-//       //   if (op->bitfields.size() != 7) continue;
-//       //   if (dynamic_cast<OpcodeBitField const&>( **itr++ ).size != 11) continue;
-//       //   {
-//       //     OperandBitField& obf = dynamic_cast<OperandBitField&>( **itr++ );
-//       //     if (obf.size != 5) continue;
-//       //     if (obf.symbol != "rm") continue;
-//       //   }
-        
-//       //   if (dynamic_cast<OpcodeBitField const&>( **itr++ ).size != 3) continue;
-//       //   {
-//       //     OperandBitField& obf = dynamic_cast<OperandBitField&>( **itr++ );
-//       //     if (obf.size != 1) continue;
-//       //     if (obf.symbol != "s") continue;
-//       //   }
-
-//       //   if (dynamic_cast<OpcodeBitField const&>( **itr++ ).size != 2) continue;
-       
-//       //   unsigned amount = strtoul( disasm_code.substr(disasm_code.size()-5,1).c_str(), 0, 10 );
-//       //   disasm_code.replace( disasm_code.size()-12,8,"DisasmRor(shift) << \"", 21 );
-        
-//       //   Variable* v = new Variable( "shift", new SourceCode( "uint8_t", FileLoc() ), new SourceCode( Str::fmt("s*%u",amount), FileLoc() ) );
-        
-//       //   op->variables.push_back( v );
-        
-//       //   // uintptr_t pos = disasm_code.find("DisasmS");
-//       //   // if (pos == std::string::npos)
-//       //   //   continue;
-      
-//       //   // disasm_code.replace( pos, 7, "DisasmD", 7 );
-        
-//       //   disasm.m_source_code->content = disasm_code.c_str();
-        
-//       // }
-//       // catch (std::bad_cast const&) { continue; }
-      
-//       // if (replace_ror_const( disasm_code ))
-//       // uintptr_t xtlpos = disasm_code.find( "xtl" );
-//       // if (xtlpos != std::string::npos)
-//       //   continue;
-      
-//       // uintptr_t end = disasm_code.find( " << \", \" << DisasmI(imm);\n" );
-//       // if (end == std::string::npos)
-//       //   continue;
-//       // disasm_code.erase(end);
-//       // std::string toto("\n  sink << \"sshll");
-//       // if (disasm_code.find(toto) != 0)
-//       //   throw 0;
-//       // disasm_code = disasm_code.substr(toto.size());
-//       // disasm_code = std::string("\n  if (imm) { sink << \"sshll") + disasm_code + " << \", \" << DisasmI(imm); } else { sink << \"sxtl" + disasm_code + "; }\n";
-      
-//       // auto&& itr = op->bitfields.begin();
+//       std::cerr << opname << DumpBitFields( op->bitfields ) << " (" << cmv.compare_op << cmv.is_reg << cmv.elements << cmv.ebytes << ")\n";
 //     }
 // }
 
