648909c3d5ab36a648d3ff7f40f0acb35a20b010
Yves Lhuillier <yves.lhuillier@cea.fr>
2019-09-13 17:01:48 +0200

    initial import of dbgate
    Former-commit-id: 3f5049e059444f637b6139959d4b7178ceb6c43d

---
+++ package/dist_dbgate.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+SIMPKG=dbgate
+SIMPKGDIR=cxx/dbgate
+source "$(dirname $0)/dist_common.sh"
+
+import unisim/util/dbgate
+import unisim/util/hypapp
+
+import libc/inttypes
+import std/iostream
+import std/sstream
+import std/string
+import std/map
+import std/vector
+
+copy source header template data
+copy m4 && has_to_build_simulator_configure=yes # Some imported files (m4 macros) impact configure generation
+
+UNISIM_LIB_SIMULATOR_SOURCE_FILES="$(files source)"
+
+UNISIM_LIB_SIMULATOR_HEADER_FILES="$(files header) $(files template)"
+
+UNISIM_LIB_SIMULATOR_M4_FILES="$(files m4)"
+
+UNISIM_LIB_SIMULATOR_DATA_FILES="\
+"
+
+UNISIM_SIMULATOR_SOURCE_FILES="\
+test.cc
+"
+
+UNISIM_SIMULATOR_HEADER_FILES="\
+"
+
+UNISIM_SIMULATOR_DATA_FILES="\
+COPYING \
+NEWS \
+ChangeLog \
+AUTHORS \
+README \
+INSTALL \
+"
+
+UNISIM_LIB_SIMULATOR_FILES="\
+${UNISIM_LIB_SIMULATOR_SOURCE_FILES} \
+${UNISIM_LIB_SIMULATOR_HEADER_FILES} \
+${UNISIM_LIB_SIMULATOR_DATA_FILES} \
+"
+
+for file in ${UNISIM_LIB_SIMULATOR_FILES}; do
+	dist_copy "${UNISIM_LIB_DIR}/${file}" "${DEST_DIR}/${file}"
+done
+
+UNISIM_SIMULATOR_FILES="\
+${UNISIM_SIMULATOR_SOURCE_FILES} \
+${UNISIM_SIMULATOR_HEADER_FILES} \
+${UNISIM_SIMULATOR_DATA_FILES} \
+"
+
+for file in ${UNISIM_SIMULATOR_FILES}; do
+	dist_copy "${UNISIM_SIMULATOR_DIR}/${file}" "${DEST_DIR}/${file}"
+done
+
+# Simulator
+
+output_simulator_configure_ac <(cat <<EOF
+AC_INIT([UNISIM INTELemu C++ simulator], [${SIMULATOR_VERSION}], [Yves Lhuillier <yves.lhuillier@cea.fr>], [unisim-${SIMPKG}])
+AC_CONFIG_MACRO_DIR([m4])
+AC_CONFIG_AUX_DIR(config)
+AC_CONFIG_HEADERS([config.h])
+AC_CANONICAL_BUILD
+AC_CANONICAL_HOST
+AC_CANONICAL_TARGET
+AM_INIT_AUTOMAKE([subdir-objects tar-pax])
+AC_PATH_PROGS(SH, sh)
+AC_PROG_CXX
+AC_PROG_INSTALL
+LT_INIT
+AC_SUBST(LIBTOOL_DEPS)
+AC_PROG_LN_S
+AC_LANG([C++])
+AM_PROG_CC_C_O
+case "\${host}" in
+	*mingw*)
+		CPPFLAGS="-U__STRICT_ANSI__ \${CPPFLAGS}"
+		;;
+	*)
+		;;
+esac
+$(lines ac)
+AX_CXXFLAGS_WARN_ALL
+AC_DEFINE([BIN_TO_SHARED_DATA_PATH], ["../share/unisim-${SIMPKG}-${SIMULATOR_VERSION}"], [path of shared data relative to bin directory])
+AC_CONFIG_FILES([Makefile])
+AC_OUTPUT
+EOF
+)
+
+output_simulator_makefile_am <(cat << EOF
+ACLOCAL_AMFLAGS=-I m4
+AM_CPPFLAGS=-I\$(top_srcdir) -I\$(top_builddir)
+LIBTOOL_DEPS = @LIBTOOL_DEPS@
+libtool: \$(LIBTOOL_DEPS)
+	\$(SHELL) ./config.status libtool
+
+# # Program
+# bin_PROGRAMS = unisim-${SIMPKG}-${SIMULATOR_VERSION}
+# unisim_${SIMPKG}_${AM_SIMULATOR_VERSION}_SOURCES = ${UNISIM_SIMULATOR_SOURCE_FILES}
+# unisim_${SIMPKG}_${AM_SIMULATOR_VERSION}_LDADD = libunisim-${SIMPKG}-${SIMULATOR_VERSION}.la
+
+
+# Dynamic Plugin
+lib_LTLIBRARIES = libunisim-${SIMPKG}-plugin-${SIMULATOR_VERSION}.la
+libunisim_${SIMPKG}_plugin_${AM_SIMULATOR_VERSION}_la_SOURCES = ${UNISIM_LIB_SIMULATOR_SOURCE_FILES} ${UNISIM_SIMULATOR_SOURCE_FILES}
+libunisim_${SIMPKG}_plugin_${AM_SIMULATOR_VERSION}_la_CPPFLAGS = -DSIM_PLUGIN
+libunisim_${SIMPKG}_plugin_${AM_SIMULATOR_VERSION}_la_LDFLAGS = -shared -no-undefined
+
+noinst_HEADERS = ${UNISIM_LIB_SIMULATOR_HEADER_FILES} ${UNISIM_SIMULATOR_HEADER_FILES}
+EXTRA_DIST = ${UNISIM_LIB_SIMULATOR_M4_FILES}
+sharedir = \$(prefix)/share/unisim-${SIMPKG}-${SIMULATOR_VERSION}
+dist_share_DATA = ${UNISIM_SIMULATOR_DATA_FILES}
+nobase_dist_share_DATA = ${UNISIM_LIB_SIMULATOR_DATA_FILES}
+
+EOF
+)
+
+build_simulator_configure
+
+echo "Distribution is up-to-date"
---
+++ unisim_simulators/cxx/dbgate/test.cc
@@ -0,0 +1,19 @@
+#include <unisim/util/dbgate/dbgate.hh>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <sstream>
+#include <cstdlib>
+#include <cstdio>
+
+int
+main(int argc, char** argv)
+{
+  std::cout << "Testing the DBGATE shared library.\n";
+  
+  return 0;
+}
