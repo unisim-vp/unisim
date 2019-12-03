798e5ee21c44aa479bdd15a6298afc7e2a75038a
Gilles Mouchard <gilles.mouchard@cea.fr>
2019-12-03 14:26:32 +0100

    Packaging: -Added support for multiple list in arguments to Functions 'files' and 'lines'
    Former-commit-id: dd994405cb91dc480b7dba47e6f35618ac0dc21a

--- package/dist_common.sh
+++ package/dist_common.sh
@@ -70,42 +70,49 @@ function import()
 
 function files()
 {
-	local LIST_NAME="$1"
-	local FOUND='no'
-	for PACKAGE in ${UNISIM_LIB_PACKAGES[@]}; do
-		LIST_FILENAME="${PACKAGE_DIR}/${PACKAGE}/${LIST_NAME}_list.txt"
-		if [ -e "${LIST_FILENAME}" ]; then
-			local FILENAME
-			while IFS= read -r FILENAME; do
-				FOUND='yes'
-				echo -n " ${FILENAME}"
-			done < "${LIST_FILENAME}"
-		fi
-	done
+	while [ $# -ne 0 ]; do
+		local LIST_NAME="$1"
+		local FOUND='no'
+		for PACKAGE in ${UNISIM_LIB_PACKAGES[@]}; do
+			LIST_FILENAME="${PACKAGE_DIR}/${PACKAGE}/${LIST_NAME}_list.txt"
+			if [ -e "${LIST_FILENAME}" ]; then
+				local FILENAME
+				while IFS= read -r FILENAME; do
+					FOUND='yes'
+					echo -n " ${FILENAME}"
+				done < "${LIST_FILENAME}"
+			fi
+		done
 	
-	if [ "${FOUND}" != 'yes' ]; then
-		echo -e "\033[33mWARNING! There are no files in List \"${LIST_NAME}\"\033[0m" >&2
-	fi
+		if [ "${FOUND}" != 'yes' ]; then
+			echo -e "\033[33mWARNING! There are no files in List \"${LIST_NAME}\"\033[0m" >&2
+		fi
+		
+		shift
+	done
 }
 
 function lines()
 {
-	local LIST_NAME="$1"
-	local FOUND='no'
-	for PACKAGE in ${UNISIM_LIB_PACKAGES[@]}; do
-		LIST_FILENAME="${PACKAGE_DIR}/${PACKAGE}/${LIST_NAME}_list.txt"
-		if [ -e "${LIST_FILENAME}" ]; then
-			local LINE
-			while IFS= read -r LINE; do
-				FOUND='yes'
-				echo "${LINE}"
-			done < "${LIST_FILENAME}"
+	while [ $# -ne 0 ]; do
+		local LIST_NAME="$1"
+		local FOUND='no'
+		for PACKAGE in ${UNISIM_LIB_PACKAGES[@]}; do
+			LIST_FILENAME="${PACKAGE_DIR}/${PACKAGE}/${LIST_NAME}_list.txt"
+			if [ -e "${LIST_FILENAME}" ]; then
+				local LINE
+				while IFS= read -r LINE; do
+					FOUND='yes'
+					echo "${LINE}"
+				done < "${LIST_FILENAME}"
+			fi
+		done
+		
+		if [ "${FOUND}" != 'yes' ]; then
+			echo -e "\033[33mWARNING! There are no lines in List \"${LIST_NAME}\"\033[0m" >&2
 		fi
+		shift
 	done
-	
-	if [ "${FOUND}" != 'yes' ]; then
-		echo -e "\033[33mWARNING! There are no lines in List \"${LIST_NAME}\"\033[0m" >&2
-	fi
 }
 
 function pkg_deps_changed()
